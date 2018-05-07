09f197775578b4d17f5d10ed76c223d504b94436
onepeople <43036456@qq.com>
2018-05-07 10:49:20 +0800

    改造

--- public/index.php
+++ public/index.php
@@ -2,7 +2,7 @@
 error_reporting(1);
 header('content-Type:text/html;charset=utf-8;');
 define('APP_PATH',  realpath(dirname(__FILE__) . '/../'));
-Yaf\Loader::import(APP_PATH.'/application/init.php');
+\Yaf\Loader::import(APP_PATH.'/application/init.php');
 $app = new Yaf\Application(APP_PATH.'/conf/application.ini');
 $app->getDispatcher()->throwException(TRUE);
 $app->bootstrap()->run();
