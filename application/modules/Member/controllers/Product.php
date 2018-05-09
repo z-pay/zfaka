c48b5facb118eb7404b3e58e33a652aa29d20306
onepeople <43036456@qq.com>
2018-05-09 16:49:42 +0800

    改造

---
+++ application/modules/Member/controllers/Product.php
@@ -0,0 +1,22 @@
+<?php
+
+/*
+ * 功能：会员中心－个人中心
+ * author:资料空白
+ * time:20180509
+ */
+
+class ProductController extends PcBasicController
+{
+
+    public function init()
+    {
+        parent::init();
+    }
+
+    public function indexAction()
+    {
+		$data = array();
+        $this->getView()->assign($data);
+    }
+}
