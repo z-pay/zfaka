467f8fdf1421cebfd06be8ed9cacc224ba56cf52
onepeople <43036456@qq.com>
2018-05-09 16:13:16 +0800

    添加

---
+++ application/modules/Member/controllers/Center.php
@@ -0,0 +1,24 @@
+<?php
+
+/*
+ * 功能：会员中心－首页
+ * author:资料空白
+ * time:20180509
+ */
+
+class CenterController extends PcBasicController
+{
+
+    public function init()
+    {
+        parent::init();
+    }
+
+    public function indexAction()
+    {
+		$data = array();
+        $this->getView()->assign($data);
+    }
+
+
+}
