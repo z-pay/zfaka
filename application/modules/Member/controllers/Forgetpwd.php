0ae57a4dbd9a079e48595e80b92f3b8c7a2fe24b
onepeople <43036456@qq.com>
2018-05-09 17:34:46 +0800

    改造

---
+++ application/modules/Member/controllers/Forgetpwd.php
@@ -0,0 +1,20 @@
+<?php
+
+/*
+ * 功能：会员中心－忘记密码类
+ * author:资料空白
+ * time:20150909
+ */
+
+class ForgetpwdController extends PcBasicController
+{
+
+    public function init()
+    {
+        parent::init();
+    }
+
+    public function indexAction()
+    {
+    }
+}
