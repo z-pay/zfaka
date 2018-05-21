43cd4f56fbab217318c9efe185d37866d387f6f5
莫正强 <43036456@qq.com>
2018-05-21 11:43:06 +0800

    登录处理

---
+++ application/model/M_User.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * File: M_User.php
+ * Functionality: 用户 model
+ * Author: 资料空白
+ * Date: 2018-05-21
+ */
+
+class M_User extends Model {
+
+	function __construct() {
+		$this->table = TB_PREFIX.'user';
+		parent::__construct();
+	}
+
+	/**
+	 * 登录
+	 * @param string $email
+	 * @param string $password
+	 * @return params on success or 0 or failure
+	 */
+	public function checkLogin($email, $password){
+		$field = array('id', 'email','password');
+		if(isEmail($email)){
+			$where = array('email' => $mobile);
+		}else{
+			return FALSE;
+		}
+		$result=$this->Field($field)->Where($where)->SelectOne();
+		if($result){
+			if(md5($password)===$result['password']){
+				unset($result['password']);
+				$this->_session($result);
+				return $result;
+			}
+		}
+		return FALSE;
+	}
+
+
+
+	/**
+	* 登录session处理
+	* @param $params
+	* @return 1
+	*/
+	private function _session($params){
+		if(!empty($params)){
+			$params['expiretime']= time() + 15*60;
+			\Yaf\Session::getInstance()->__set('uinfo', $params);
+		}
+	}
+
+}
--- application/modules/Member/controllers/Login.php
+++ application/modules/Member/controllers/Login.php
@@ -8,10 +8,12 @@
 
 class LoginController extends PcBasicController
 {
-
+	private $m_user;
+	
     public function init()
     {
         parent::init();
+		$this->m_user = $this->load('user');
     }
 
     public function indexAction()
@@ -20,15 +22,36 @@ class LoginController extends PcBasicController
             $this->redirect("/member/center/");
             return FALSE;
         }
+		
         //对refererUrl进行有效性校验
         $referer_url = $this->get('referer_url', false);
-        $sign = $this->get('sign', false);
+        $sign        = $this->get('sign', false);
+		
         if (md5(URL_KEY . $referer_url) === $sign) {
             $data['referer_url'] = $referer_url;
         } else {
             $data['referer_url'] = '';
         }
-        $data['cookie_mobile'] = $this->getCookie('mobile');
+        $data['cookie_email'] = $this->getCookie('email');
         $this->getView()->assign($data);
     }
+	
+	public function ajaxAction()
+	{
+		$email    = $this->getPost('email',false);
+		$password = $this->getPost('password',false);
+		if($email AND $password){
+			$checkUser = $this->m_user->checkLogin($email,$password);
+			if($checkUser){
+				$this->redirect("/member/center/");
+				return FALSE;
+			}else{
+				echo "登录失败";
+				exit();
+			}
+		}else{
+			echo "参数错误";
+			exit();
+		}
+	}
 }
