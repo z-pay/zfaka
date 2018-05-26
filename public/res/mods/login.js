86ef0a6248633b490d26c47e8dbd8923e11e6224
djl <276849899@qq.com>
2018-05-26 17:22:01 +0800

    登录功能

--- public/res/mods/login.js
+++ public/res/mods/login.js
@@ -4,17 +4,33 @@ layui.define(['layer', 'form'], function(exports){
 	var form = layui.form;
 
 	form.verify({
-		passwd: [/^[\S]{6,12}$/,'密码必须6到12位，且不能出现空格'],
+		passwd: [/^[\S]{6,20}$/,'密码必须6到20位，除空格外的任意字符'],
 		vercode: [/^[0-9a-zA-Z]{4}$/,'图形验证码错误']
 	});
-	form.on('submit(login)', function(data){
-		console.log(data.elem) //被执行事件的元素DOM对象，一般为button对象
-		console.log(data.form) //被执行提交的form对象，一般在存在form标签时才会返回
-		console.log(data.field) //当前容器的全部表单字段，名值对形式：{name: value}
 
-		layer.alert(JSON.stringify(data.field), {
-			title: '最终的提交信息'
+	form.on('submit(login)', function(data){
+
+		data.field.csrf_token = TOKEN;
+		var i = layer.load(1);
+		$.ajax({
+			url: '/member/login/ajax/',
+			type: 'POST',
+			dataType: 'json',
+			data: data.field,
 		})
+		.done(function(res) {
+			if (res.code == '1') {
+				location.pathname = '/member'
+			} else {
+				layer.msg(res.msg,{icon:1});
+			}
+		})
+		.fail(function() {
+			layer.msg('服务器连接失败，请联系管理员',{icon:1});
+		})
+		.always(function() {
+			layer.close(i);
+		});
 
 		return false; //阻止表单跳转。如果需要表单跳转，去掉这段即可。
 	});
