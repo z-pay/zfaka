707a378c17b99bbec462b5a422e0b920706cae57
莫正强 <43036456@qq.com>
2018-05-28 15:20:03 +0800

    改造

--- application/init.php
+++ application/init.php
@@ -21,7 +21,7 @@ define('CRONTAB_FILE',APP_PATH.'/log/crontab/'.CUR_DATE.'.log');
 
 error_reporting(1);
 error_reporting(E_ALL ^E_NOTICE);
-ini_set('display_errors', 'off');
+ini_set('display_errors', 'on');
 ini_set('display_startup_errors',1);//php启动错误信息
 ini_set('log_errors', 'on');
 ini_set('error_log', LOG_FILE);
--- application/model/M_Seo.php
+++ application/model/M_Seo.php
@@ -34,10 +34,7 @@ class M_Seo extends Model {
     		$data['config'] = $config = $this->_getData();
     		$data['expire_time'] = time() + 600;
     		
-    			$fp = fopen($file_path, "w");
-	    		fwrite($fp, json_encode($data));
-	    		fclose($fp);
-    
+			file_put_contents($file_path,json_encode($data));
     	}
 		
 
