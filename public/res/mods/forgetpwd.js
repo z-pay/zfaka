283e5ae8073222d68fe7509818828feeaa91ea1d
丁江磊 <276849899@qq.com>
2018-05-29 10:37:47 +0800

    找回密码

--- public/res/mods/forgetpwd.js
+++ public/res/mods/forgetpwd.js
@@ -9,6 +9,13 @@ layui.define(['layer', 'form'], function(exports){
 	});
 
 	form.verify({
+		passwd: [/^[\S]{6,16}$/,'密码必须6到16位，除空格外的任意字符'],
+		repasswd: function(value){
+			var passwd = $('#L_pass').val();
+			if (value!=passwd) {
+				return '两次输入的密码不一致';
+			}
+		},
 		vercode: [/^[0-9a-zA-Z]{4}$/,'图形验证码错误']
 	});
 
@@ -27,6 +34,33 @@ layui.define(['layer', 'form'], function(exports){
 				$('.layui-tab-item').removeClass('layui-show').eq(':last').addClass('layui-show');
 			} else {
 				$('.loadcode').attr('src','/Captcha?t=forgetpwd&n=' + Math.random());
+				layer.msg(res.msg,{icon:2,time:5000});
+			}
+		})
+		.fail(function() {
+			layer.msg('服务器连接失败，请联系管理员',{icon:2,time:5000});
+		})
+		.always(function() {
+			layer.close(i);
+		});
+
+		return false; //阻止表单跳转。如果需要表单跳转，去掉这段即可。
+	});
+
+	form.on('submit(reset)', function(data){
+
+		data.field.csrf_token = TOKEN;
+		var i = layer.load(2,{shade: [0.5,'#fff']});
+		$.ajax({
+			url: '/member/forgetpwd/resetajax/',
+			type: 'POST',
+			dataType: 'json',
+			data: data.field,
+		})
+		.done(function(res) {
+			if (res.code == '1') {
+				location.href = '/member/login/';
+			} else {
 				layer.msg(res.msg,{icon:2,time:5000});
 			}
 		})
