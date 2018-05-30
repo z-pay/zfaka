a8877de6c6eca61790a3dc4d2fdbdc85cfeb6d7e
莫正强 <43036456@qq.com>
2018-05-30 17:35:47 +0800

    修改密码

--- application/modules/Member/controllers/Profiles.php
+++ application/modules/Member/controllers/Profiles.php
@@ -61,6 +61,50 @@ class ProfilesController extends PcBasicController
         $this->getView()->assign($data);
 	}
 	
+	public function passwordajaxAction(){
+		$password = $this->getPost('password',false);
+		$oldpassword = $this->getPost('oldpassword',false);
+		$csrf_token = $this->getPost('csrf_token', false);
+		
+		$data = array();
+		
+        if ($this->login==FALSE AND !$this->userid) {
+            $data = array('code' => 1000, 'msg' => '请登录');
+			Helper::response($data);
+        }
+		
+		if($password AND $oldpassword AND $csrf_token){
+			if ($this->VerifyCsrfToken($csrf_token)) {
+				if ($oldpassword !== $password) {
+					if (strlen($password) < 6 ) {
+						$data = array('code' => 1002, 'msg' => '密码过于简单,密码至少6位');
+					} else {
+						$check = $this->m_user->checkLogin($this->uinfo['email'], $oldpassword);
+						if ($check) {
+
+								$update = $this->m_user->changePWD($this->userid, $password);
+								if ($update) {
+									$data = array('code' => 1, 'msg' => '修改密码');
+								} else {
+									$data = array('code' => 1004, 'msg' => '数据更新异常');
+								}
+
+						} else {
+							$data = array('code' => 1003, 'msg' => '原始密码不正确');
+						}
+					}
+				} else {
+					$data = array('code' => 1001, 'msg' => '新旧密码不能相同');
+				}
+			} else {
+                $data = array('code' => 1001, 'msg' => '页面超时，请刷新页面后重试!');
+            }
+		}else{
+			$data = array('code' => 1000, 'msg' => '丢失参数');
+		}
+		Helper::response($data);
+	}
+	
 	public function thirdloginAction(){
 		$data = array();
         $this->getView()->assign($data);
