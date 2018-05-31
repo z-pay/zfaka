e432c23d9aae7d7bbbe137e22d929783d0bf863d
djl <276849899@qq.com>
2018-05-31 21:23:09 +0800

    test

--- public/res/mods/help.js
+++ public/res/mods/help.js
@@ -1,70 +1,22 @@
 
-layui.define(['layer', 'form'], function(exports){
+layui.define(['layer', 'table', 'form'], function(exports){
 	var $ = layui.jquery;
 	var layer = layui.layer;
+	var form = layui.table;
 	var form = layui.form;
 
 
-
-
-	$.ajax({
-		url: '/help/index/ajax',
-		type: 'GET',
-		dataType: 'json'
-	})
-	.done(function(res) {
-		console.log(res);
-	})
-	.fail(function() {
-		console.log("error");
-	})
-	.always(function() {
-		console.log("complete");
+	table.render({
+		elem: '#question',
+		url: '/demo/table/user/',
+		page: true,
+		cols: [[
+			{field: 'title', title: 'ID', width:80, sort: true, fixed: 'left'},
+			{field: 'addtime', title: '用户名', width:80}
+		]]
 	});
 
-	$('.loadcode').on('click', function(event) {
-		event.preventDefault();
-		$(this).attr('src','/Captcha?t=register&n=' + Math.random());
-	});
 
-	form.verify({
-		passwd: [/^[\S]{6,16}$/,'密码必须6到16位，除空格外的任意字符'],
-		repasswd: function(value){
-			var passwd = $('#L_pass').val();
-			if (value!=passwd) {
-				return '两次输入的密码不一致';
-			}
-		},
-		vercode: [/^[0-9a-zA-Z]{4}$/,'图形验证码错误']
-	});
-
-	form.on('submit(register)', function(data){
-
-		data.field.csrf_token = TOKEN;
-		var i = layer.load(2,{shade: [0.5,'#fff']});
-		$.ajax({
-			url: data.form.action,
-			type: data.form.method,
-			dataType: 'json',
-			data: data.field,
-		})
-		.done(function(res) {
-			if (res.code == '1') {
-				location.pathname = '/member'
-			} else {
-				$('.loadcode').attr('src','/Captcha?t=register&n=' + Math.random());
-				layer.msg(res.msg,{icon:2,time:5000});
-			}
-		})
-		.fail(function() {
-			layer.msg('服务器连接失败，请联系管理员',{icon:2,time:5000});
-		})
-		.always(function() {
-			layer.close(i);
-		});
-
-		return false; //阻止表单跳转。如果需要表单跳转，去掉这段即可。
-	});
 
 	exports('help',null)
 });
