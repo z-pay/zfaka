2ba853d69514ebc2a20248e8bf2c9aaf9fa2983a
丁江磊 <276849899@qq.com>
2018-05-31 10:09:15 +0800

    修改密码

--- public/res/mods/profiles.js
+++ public/res/mods/profiles.js
@@ -4,6 +4,19 @@ layui.define(['layer', 'form'], function(exports){
 	var form = layui.form;
 
 	form.verify({
+		passwd: [/^[\S]{6,16}$/,'密码必须6到16位，除空格外的任意字符'],
+		passwdnew: function(value){
+			var passwd = $('#L_nowpass').val();
+			if (value==passwd) {
+				return '新密码与当前密码不能相同';
+			}
+		},
+		repasswd: function(value){
+			var passwd = $('#L_pass').val();
+			if (value!=passwd) {
+				return '两次输入的密码不一致';
+			}
+		},
 		qq:function(value){
 			if (value != ''&& !/^[1-9][0-9]{5,10}$/.test(value)) {
 				return '请输入正确的QQ号码';
@@ -11,6 +24,7 @@ layui.define(['layer', 'form'], function(exports){
 		}
 	});
 
+	//修改资料
 	form.on('submit(profiles)', function(data){
 
 		data.field.csrf_token = TOKEN;
@@ -46,6 +60,44 @@ layui.define(['layer', 'form'], function(exports){
 		});
 
 		return false; //阻止表单跳转。如果需要表单跳转，去掉这段即可。
+	});
+
+	//修改密码
+	form.on('submit(pass)', function(data){
+
+		data.field.csrf_token = TOKEN;
+		var i = layer.load(2,{shade: [0.5,'#fff']});
+		$.ajax({
+			url: '/member/profiles/passwordajax',
+			type: 'POST',
+			dataType: 'json',
+			data: data.field,
+		})
+		.done(function(res) {
+			if (res.code == '1') {
+				layer.open({
+					title: '提示',
+					content: res.msg,
+					btn: ['确定'],
+					yes: function(index, layero){
+					    location.reload();
+					},
+					cancel: function(){ 
+					    location.reload();
+					}
+				});
+			} else {
+				layer.msg(res.msg,{icon:2,time:5000});
+			}
+		})
+		.fail(function() {
+			layer.msg('服务器连接失败，请联系管理员',{icon:2,time:5000});
+		})
+		.always(function() {
+			layer.close(i);
+		});
+
+		return false; //阻止表单跳转。
 	});
 
 	exports('profiles',null)
