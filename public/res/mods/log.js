ef74f10d89c90a03b214cbfa843c32167ac2b8ca
djl <276849899@qq.com>
2018-06-03 21:43:52 +0800

    登录日志

--- public/res/mods/log.js
+++ public/res/mods/log.js
@@ -10,9 +10,9 @@ layui.define(['layer', 'table'], function(exports){
 		page: true,
 		cellMinWidth:60,
 		cols: [[
-			{field: 'id', title: 'ID'},
+			{field: 'id', title: 'ID', width:80},
 			{field: 'ip', title: '登录IP', minWidth:160},
-			{field: 'addtime', title: '登录时间', width:160, templet: '#addtime',align:'center'}
+			{field: 'addtime', title: '登录时间', width:200, templet: '#addtime',align:'center'}
 		]]
 	});
 
