abb88d2348f6f9fa20b1ebca6848ec150531e6a9
djl <276849899@qq.com>
2018-06-04 21:55:39 +0800

    test

--- public/res/mods/help.js
+++ public/res/mods/help.js
@@ -13,7 +13,7 @@ layui.define(['layer', 'table', 'element'], function(exports){
 		cols: [[
 			{field: 'title', title: '常见问题'},
 			{field: 'addtime', title: '发布时间', width:160, templet: '#addtime',align:'center'},
-			{fixed:'right', title: '详情', width:100, templet: '#details', align:'center'}
+			{fixed: 'right', title: '详情', width:100, align:'center', templet: function(d){return 'ID：'+ d.id +'，标题：<span style="color: #c00;">'+ d.title +'</span>'}}
 		]]
 	});
 
