81d94f458c193e023f6681befd4712626e7e05bc
djl <276849899@qq.com>
2018-06-04 21:17:47 +0800

    help详情

--- public/res/mods/help.js
+++ public/res/mods/help.js
@@ -12,7 +12,7 @@ layui.define(['layer', 'table', 'util'], function(exports){
 		page: true,
 		cols: [[
 			{field: 'title', title: '常见问题'},
-			{field: 'addtime', title: '发布时间', width:160, templet: '#addtime',align:'center'}
+			{field: 'addtime', title: '发布时间', width:160, templet: '#addtime',align:'center'},
 			{title: '详情', width:100, align:'center', toolbar: '#toolbar'}
 		]]
 	});
