519d8c152807427811978e1de8565dad51940e28
zlkb <43036456@qq.com>
2018-06-07 22:56:10 +0800

    i[date

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -1,20 +1,24 @@
 <?php
-class Bootstrap extends Yaf\Bootstrap_Abstract{
+class Bootstrap extends \Yaf\Bootstrap_Abstract{
     public function _initCore(){
 		ini_set('yaf.library', LIB_PATH);
-		Yaf\Loader::import(CORE_PATH.'/Helper.php');
-        Yaf\Loader::import(CORE_PATH.'/Basic.php');
-		Yaf\Loader::import(CORE_PATH.'/Model.php');
-		Yaf\Loader::import(CORE_PATH.'/BasicPc.php');
-		Yaf\Loader::import(FUNC_PATH.'/F_Basic.php');
-		Yaf\Loader::import(FUNC_PATH.'/F_Network.php');
+		\Yaf\Loader::import(CORE_PATH.'/Helper.php');
+        \Yaf\Loader::import(CORE_PATH.'/Basic.php');
+		\Yaf\Loader::import(CORE_PATH.'/Model.php');
+		\Yaf\Loader::import(CORE_PATH.'/BasicPc.php');
+		\Yaf\Loader::import(FUNC_PATH.'/F_Basic.php');
+		\\Yaf\Loader::import(FUNC_PATH.'/F_Network.php');
 		Yaf\Loader::import(FUNC_PATH.'/F_String.php');
-		Yaf\Loader::import(FUNC_PATH.'/F_Validate.php');
+		\Yaf\Loader::import(FUNC_PATH.'/F_Validate.php');
     }
-
+	
+	public function _initLoader()
+	{
+		\Yaf\Loader::import(APP_PATH . '/vendor/autoload.php');
+	}
     public function _initRoute() {}
 	
-    public function _initPlugin(Yaf\Dispatcher $dispatcher) {
+    public function _initPlugin(\Yaf\Dispatcher $dispatcher) {
         $router = new RouterPlugin();
         $dispatcher->registerPlugin($router);
 		
