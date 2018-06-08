d1b34a29e5c71072c3440da2773137bb20054705
莫正强 <43036456@qq.com>
2018-06-08 11:23:38 +0800

    update

--- public/res/mods/memberproduct.js
+++ public/res/mods/memberproduct.js
@@ -11,11 +11,11 @@ layui.define(['layer', 'table'], function(exports){
 		cellMinWidth:60,
 		cols: [[
 			{field: 'id', title: 'ID', width:80},
-			{field: 'productname', title: '订单名称', minWidth:160},
-			{field: 'price', title: '单价', minWidth:160},
-			{field: 'number', title: '数量', minWidth:160},
-			{field: 'money', title: '金额', minWidth:160},
-			{field: 'status', title: '状态', minWidth:160},
+			{field: 'productname', title: '订单名称'},
+			{field: 'price', title: '单价'},
+			{field: 'number', title: '数量', width:40},
+			{field: 'money', title: '金额'},
+			{field: 'status', title: '状态'},
 			{field: 'addtime', title: '下单时间', width:200, templet: '#addtime',align:'center'}
 		]]
 	});
