673c4e581fb2b11ea6b559c69e4cb9b334bdf9cd
莫正强 <43036456@qq.com>
2018-06-08 14:55:01 +0800

    update

--- application/function/F_String.php
+++ application/function/F_String.php
@@ -31,7 +31,41 @@ if ( ! function_exists('getRandom')){
         return $output;
     }
 }
+/**************************************************************
+*
+*  将数组转换为JSON字符串（兼容中文）
+*  @param  array   $array      要转换的数组
+*  @return string      转换得到的json字符串
+*  @access public
+*
+*************************************************************/
+if ( ! function_exists('arrayRecursive')){
+function arrayRecursive(&$array, $function, $apply_to_keys_also = false){
+	static $recursive_counter = 0;
+	if (++$recursive_counter > 1000) {
+		die('possible deep recursion attack');
+	}
 
+	if($array){
+		foreach ($array as $key => $value) {
+			if (is_array($value)) {
+				arrayRecursive($array[$key], $function, $apply_to_keys_also);
+			} else {
+				$array[$key] = $function($value);
+			}
+
+			if ($apply_to_keys_also && is_string($key)) {
+				$new_key = $function($key);
+				if ($new_key != $key) {
+					$array[$new_key] = $array[$key];
+					unset($array[$key]);
+				}
+			}
+	    }
+	}
+    $recursive_counter--;
+}
+}
 if ( ! function_exists('JSON')){
 function JSON($array) {
 	arrayRecursive($array, 'urlencode', TRUE);
