56bb1cb2c57614587898596e1d5d3e91e3183663
莫正强 <43036456@qq.com>
2018-06-08 10:20:14 +0800

    update

--- public/res/mods/productpay.js
+++ public/res/mods/productpay.js
@@ -4,11 +4,12 @@
 	
 	$('.layui-btn').on('click', function(event) {
 		event.preventDefault();
+		var paymethod = $(this).attr("data-type");
         $.ajax({
             type: "POST",
             dataType: "json",
             url: "/product/order/payajax",
-            data: { "csrf_token": TOKEN },
+            data: { "csrf_token": TOKEN,'paymethod':paymethod },
             success: function(data) {
                 if (data.code == 1) {
                     location.href = location.protocol + "//" +data.data ;
