63f993e67848fb6fe6b68670399809a7dca9dc02
莫正强 <43036456@qq.com>
2018-06-08 09:56:18 +0800

    update

--- application/modules/Product/controllers/Order.php
+++ application/modules/Product/controllers/Order.php
@@ -29,51 +29,57 @@ class OrderController extends PcBasicController
 		$number = $this->getPost('number');
 		$email = $this->getPost('email');
 		$chapwd = $this->getPost('chapwd');
-		if(is_numeric($pid) AND $pid>0 AND is_numeric($number) AND $number>0 AND $email AND isEmail($email) AND $chapwd){
-			$product = $this->m_products->Where(array('id'=>$pid))->SelectOne();
-			if(!empty($product)){
-				if($product['stockcontrol']==1 AND $product['qty']<1){
-					$data = array('code' => 1002, 'msg' => '库存不足');
-				}else{
-					//进行同一ip，下单未付款的处理判断
-					$myip = getClientIP();
-					$total = $this->m_order->Where(array('ip'=>$myip,'status'=>0))->Total();
-					if($total>1){
-						$data = array('code' => 1003, 'msg' => '处理失败,您有太多未付款订单了');
+		$csrf_token = $this->getPost('csrf_token', false);
+		
+		if(is_numeric($pid) AND $pid>0 AND is_numeric($number) AND $number>0 AND $email AND isEmail($email) AND $chapwd AND $csrf_token){
+			if ($this->VerifyCsrfToken($csrf_token)) {
+				$product = $this->m_products->Where(array('id'=>$pid))->SelectOne();
+				if(!empty($product)){
+					if($product['stockcontrol']==1 AND $product['qty']<1){
+						$data = array('code' => 1002, 'msg' => '库存不足');
 					}else{
-						//记录用户uid
-						if($this->login AND $this->userid){
-							$userid = $this->userid;
+						//进行同一ip，下单未付款的处理判断
+						$myip = getClientIP();
+						$total = $this->m_order->Where(array('ip'=>$myip,'status'=>0))->Total();
+						if($total>1){
+							$data = array('code' => 1003, 'msg' => '处理失败,您有太多未付款订单了');
 						}else{
-							$uinfo = $this->m_user->Where(array('email'=>$email))->SelectOne();
-							if(!empty($uinfo)){
-								$userid = $uinfo['id'];
+							//记录用户uid
+							if($this->login AND $this->userid){
+								$userid = $this->userid;
 							}else{
-								$userid = 0;
+								$uinfo = $this->m_user->Where(array('email'=>$email))->SelectOne();
+								if(!empty($uinfo)){
+									$userid = $uinfo['id'];
+								}else{
+									$userid = 0;
+								}
 							}
+							
+							$m=array(
+								'userid'=>$userid,
+								'email'=>$email,
+								'pid'=>$pid,
+								'productname'=>$product['name'],
+								'price'=>$product['price'],
+								'number'=>$number,
+								'money'=>$product['price']*$number,
+								'chapwd'=>$chapwd,
+								'ip'=>$myip,
+								'status'=>0,
+								'addtime'=>time(),
+							);
+							$id=$this->m_order->Insert($m);
+							$orderid = base64_encode($id);
+							$data = array('code' => 1, 'msg' => '下单成功','data'=>array('orderid'=>$orderid));
 						}
-						
-						$m=array(
-							'userid'=>$userid,
-							'email'=>$email,
-							'pid'=>$pid,
-							'productname'=>$product['name'],
-							'price'=>$product['price'],
-							'number'=>$number,
-							'money'=>$product['price']*$number,
-							'chapwd'=>$chapwd,
-							'ip'=>$myip,
-							'status'=>0,
-							'addtime'=>time(),
-						);
-						$id=$this->m_order->Insert($m);
-						$orderid = base64_encode($id);
-						$data = array('code' => 1, 'msg' => '下单成功','data'=>array('orderid'=>$orderid));
 					}
+				}else{
+					$data = array('code' => 1001, 'msg' => '商品不存在');
 				}
-			}else{
-				$data = array('code' => 1001, 'msg' => '商品不存在');
-			}
+			} else {
+                $data = array('code' => 1001, 'msg' => '页面超时，请刷新页面后重试!');
+            }
 		}else{
 			$data = array('code' => 1000, 'msg' => '丢失参数');
 		}
