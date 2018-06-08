998a5ad4a43e149b742ac79075ce705dff4f8f0b
莫正强 <43036456@qq.com>
2018-06-08 11:08:50 +0800

    update

--- application/modules/Member/controllers/Profiles.php
+++ application/modules/Member/controllers/Profiles.php
@@ -109,13 +109,5 @@ class ProfilesController extends PcBasicController
 		}
 		Helper::response($data);
 	}
-	
-	public function thirdloginAction(){
-        if ($this->login==FALSE AND !$this->userid) {
-            $this->redirect("/member/login");
-            return FALSE;
-        }
-		$data = array();
-        $this->getView()->assign($data);
-	}
+
 }
