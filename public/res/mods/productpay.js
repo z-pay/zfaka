8a29c1457fc85c44b55d032883fe09c9a0af1b24
莫正强 <43036456@qq.com>
2018-06-08 10:13:28 +0800

    update

---
+++ public/res/mods/productpay.js
@@ -0,0 +1,24 @@
+﻿layui.define(['layer', 'form'], function(exports){
+	var $ = layui.jquery;
+	var layer = layui.layer;
+	
+	$('.layui-btn').on('click', function(event) {
+		event.preventDefault();
+        $.ajax({
+            type: "POST",
+            dataType: "json",
+            url: "/product/order/payajax",
+            data: { "csrf_token": TOKEN },
+            success: function(data) {
+                if (data.code == 1) {
+                    location.href = location.protocol + "//" +res.data ;
+                } else {
+                    layer.msg(res.msg,{icon:2,time:5000});
+                }
+                return;
+            }
+        });
+	});
+
+	exports('productpay',null)
+});
