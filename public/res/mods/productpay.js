491c968df27194baacfc044529ceb4670e51ed84
莫正强 <43036456@qq.com>
2018-06-08 12:46:19 +0800

    update

--- public/res/mods/productpay.js
+++ public/res/mods/productpay.js
@@ -13,25 +13,15 @@
             data: { "csrf_token": TOKEN,'paymethod':paymethod,'orderid':orderid },
             success: function(data) {
                 if (data.code == 1) {
-					layer.open({
-						type: 1
-						,offset: 'auto'
-						,id: 'layerDemoauto' //防止重复弹出
-						,content: '<div style="padding: 20px 100px;"><img src="/product/order/showqr/?url='+data.data+'" alt="当面付" width="230" height="230"></div>'
-						,btn: '关闭'
-						,btnAlign: 'c' //按钮居中
-						,shade: 0 //不显示遮罩
-						,yes: function(){
-						  layer.closeAll();
-						}
-					});
+					
                 } else {
 					var url = "https://qr.alipay.com/bax08878zjx7cgoce6qa60ea";
 					layer.open({
 						type: 1
+						,title: false
 						,offset: 'auto'
 						,id: 'layerDemoauto' //防止重复弹出
-						,content: '<div style="padding: 20px 100px;"><img src="/product/order/showqr/?url='+url+'" alt="当面付" width="230" height="230"></div>'
+						,content: '<div style="padding: 20px 100px;"><img src="/product/order/showqr/?url='+url+'" alt="当面付" width="230" height="230"><p>请使用手机支付宝扫一扫</p><p>扫描二维码完成支付</p></div>'
 						,btn: '关闭'
 						,btnAlign: 'c' //按钮居中
 						,shade: 0 //不显示遮罩
