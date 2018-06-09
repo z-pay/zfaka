5c2ef25213f8f3e944e2ab1211e3044a8123559e
zlkb <43036456@qq.com>
2018-06-09 15:35:26 +0800

    update

--- application/controllers/Index.php
+++ application/controllers/Index.php
@@ -13,4 +13,16 @@ class IndexController extends PcBasicController {
 		}
 		return FALSE;
 	}
+	
+	public function testAction(){
+		$paymethod='zfbf2f';
+		$this->m_payment = $this->load('payment');
+		$payments = $this->m_payment->getConfig();
+		
+			$payconfig = $payments[$paymethod];
+			//支付宝公钥，账户中心->密钥管理->开放平台密钥，找到添加了支付功能的应用，根据你的加密类型，查看支付宝公钥
+			echo $alipayPublicKey=$payconfig['userid'];
+			$aliPay = new \Pay\AlipayService($alipayPublicKey);	
+var_dump($aliPay);			
+	}
 }
