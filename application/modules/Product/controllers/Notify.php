436c071b002f305abe49bfa98b57b09302083197
zlkb <43036456@qq.com>
2018-06-09 15:48:53 +0800

    update

--- application/modules/Product/controllers/Notify.php
+++ application/modules/Product/controllers/Notify.php
@@ -25,32 +25,36 @@ class NotifyController extends PcBasicController
 
     public function indexAction()
     {
-		file_put_contents(YEWU_FILE, CUR_DATETIME.'-'.json_encode($_POST).PHP_EOL, FILE_APPEND);
-		$data = array();
-		$paymethod = $this->get('paymethod',false);
-		$payments = $this->m_payment->getConfig();
-		if($paymethod=='zfbf2f'){
-			$payconfig = $payments[$paymethod];
-			//支付宝公钥，账户中心->密钥管理->开放平台密钥，找到添加了支付功能的应用，根据你的加密类型，查看支付宝公钥
-			$alipayPublicKey=$payconfig['appid'];
-			$aliPay = new \Pay\AlipayService($alipayPublicKey);
-			//验证签名
-			$result = $aliPay->rsaCheck($_POST,$_POST['sign_type']);
-			if($result===true){
-				if($_POST['trade_status'] == "TRADE_SUCCESS"){
-					$params = array('paymethod'=>$paymethod,'tradeid'=>$_POST['trade_no'],'orderid'=>$_POST['out_trade_no'],'paymoney'=>$_POST['total_amount']);
-					$this->_doOrder($params);
-					//处理你的逻辑，例如获取订单号$_POST['out_trade_no']，订单金额$_POST['total_amount']等
-					//程序执行完后必须打印输出“success”（不包含引号）。如果商户反馈给支付宝的字符不是success这7个字符，支付宝服务器会不断重发通知，直到超过24小时22分钟。一般情况下，25小时以内完成8次通知（通知的间隔频率一般是：4m,10m,10m,1h,2h,6h,15h）；
-					echo 'success';exit();
+		if(!empty($_POST)){
+			file_put_contents(YEWU_FILE, CUR_DATETIME.'-'.json_encode($_POST).PHP_EOL, FILE_APPEND);
+			$data = array();
+			$paymethod = $this->get('paymethod',false);
+			$payments = $this->m_payment->getConfig();
+			if($paymethod=='zfbf2f'){
+				$payconfig = $payments[$paymethod];
+				//支付宝公钥，账户中心->密钥管理->开放平台密钥，找到添加了支付功能的应用，根据你的加密类型，查看支付宝公钥
+				$alipayPublicKey=$payconfig['appid'];
+				$aliPay = new \Pay\AlipayService($alipayPublicKey);
+				//验证签名
+				$result = $aliPay->rsaCheck($_POST,$_POST['sign_type']);
+				if($result===true){
+					if($_POST['trade_status'] == "TRADE_SUCCESS"){
+						$params = array('paymethod'=>$paymethod,'tradeid'=>$_POST['trade_no'],'orderid'=>$_POST['out_trade_no'],'paymoney'=>$_POST['total_amount']);
+						$this->_doOrder($params);
+						//处理你的逻辑，例如获取订单号$_POST['out_trade_no']，订单金额$_POST['total_amount']等
+						//程序执行完后必须打印输出“success”（不包含引号）。如果商户反馈给支付宝的字符不是success这7个字符，支付宝服务器会不断重发通知，直到超过24小时22分钟。一般情况下，25小时以内完成8次通知（通知的间隔频率一般是：4m,10m,10m,1h,2h,6h,15h）；
+						echo 'success';exit();
+					}else{
+						echo 'error';exit();
+					}
 				}else{
-					echo 'erro47r';exit();
+					echo 'error';exit();
 				}
 			}else{
-				echo 'erro50r';exit();
+				echo 'error';exit();
 			}
 		}else{
-			echo 'error53';exit();
+			echo 'error';exit();
 		}
     }
 	
