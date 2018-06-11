173adbe943a425d626b2ac787faa34d76628f706
莫正强 <43036456@qq.com>
2018-06-11 11:41:17 +0800

    update

--- application/library/Pay/zfbf2f.php
+++ application/library/Pay/zfbf2f.php
@@ -40,7 +40,6 @@ class zfbf2f implements PayNotifyInterface
 	public function notifyProcess(array $params)
 	{
 		$m_order = \Helper::import('order');
-		$m_payment = \Helper::import('payment');
 		$m_products_card = \Helper::import('products_card');
 		$m_email_queue = \Helper::import('email_queue');
 		$m_products = \Helper::import('products');
@@ -49,16 +48,16 @@ class zfbf2f implements PayNotifyInterface
 			if($params['body']=='zfbf2f'){
 				//1.先更新支付总金额
 				$update = array('status'=>1,'paytime'=>time(),'tradeid'=>$params['transaction_id'],'paymethod'=>$params['body'],'paymoney'=>$params['amount']);
-				$this->m_order->Where(array('orderid'=>$params['order_no'],'status'=>0))->Update($update);
+				$m_order->Where(array('orderid'=>$params['order_no'],'status'=>0))->Update($update);
 				
 				//2.检查是否属于自动发卡产品,如果是就自动发卡
 				//---2.1通过orderid,查询order订单
-				$order = $this->m_order->Where(array('orderid'=>$params['order_no']))->SelectOne();
+				$order = $m_order->Where(array('orderid'=>$params['order_no']))->SelectOne();
 				if(!empty($order)){
 					if($order['auto']>0){
 						//自动处理
 						//2.2查询通过订单中记录的pid，根据购买数量查询卡密
-						$cards = $this->m_products_card->Where(array('pid'=>$order['pid'],'oid'=>0))->Limit($order['number'])->Select();
+						$cards = $m_products_card->Where(array('pid'=>$order['pid'],'oid'=>0))->Limit($order['number'])->Select();
 						if(is_array($cards) AND !empty($cards) AND count($cards)==$order['number']){
 							//2.3已经获取到了对应的卡id,卡密
 							$card_mi_array = array_column($cards, 'card');
@@ -68,22 +67,22 @@ class zfbf2f implements PayNotifyInterface
 							$card_id_str = implode(',',$card_id_array);
 							
 							//2.4直接进行卡密与订单的关联
-							$this->m_order->Where("id in ({$card_id_str})")->Where(array('oid'=>0))->Update(array('oid'=>$order['id']));
+							$m_order->Where("id in ({$card_id_str})")->Where(array('oid'=>0))->Update(array('oid'=>$order['id']));
 							//2.5然后进行库存清减
 							$qty_m = array('qty' => 'qty-'.$order['number']);
-							$this->m_products->Where(array('id'=>$order['pid']))->Update($qty_m,TRUE);	
+							$m_products->Where(array('id'=>$order['pid']))->Update($qty_m,TRUE);	
 							//2.6 把邮件通知写到消息队列中，然后用定时任务去执行即可
 							$content = '用户:' . $email . ',购买的产品'.$order['productname'].',卡密是:'.$card_id_str;
 							$m=array('email'=>$order['email'],'subject'=>'卡密发送','content'=>$content,'addtime'=>time(),'status'=>0);
-							$this->m_email_queue->Insert($m);
+							$m_email_queue->Insert($m);
 						}else{
 							//这里说明库存不足了，干脆就什么都不处理，直接记录异常，同时更新订单状态
-							$this->m_order->Where(array('orderid'=>$params['order_no'],'status'=>1))->Update(array('status'=>3));
+							$m_order->Where(array('orderid'=>$params['order_no'],'status'=>1))->Update(array('status'=>3));
 							file_put_contents(YEWU_FILE, CUR_DATETIME.'-'.'库存不足，无法处理'.PHP_EOL, FILE_APPEND);
 							//把邮件通知写到消息队列中，然后用定时任务去执行即可
 							$content = '用户:' . $email . ',购买的产品'.$order['productname'].',由于库存不足暂时无法处理,管理员正在拼命处理中....请耐心等待!';
 							$m=array('email'=>$order['email'],'subject'=>'卡密发送','content'=>$content,'addtime'=>time(),'status'=>0);
-							$this->m_email_queue->Insert($m);
+							$m_email_queue->Insert($m);
 						}
 					}else{
 						//手工操作，这里暂时不处理	
