435ef3cb87d06f0a5d3e708e8d97d042c52093c2
莫正强 <43036456@qq.com>
2018-06-11 15:45:15 +0800

    update

--- public/res/mods/productquery.js
+++ public/res/mods/productquery.js
@@ -45,7 +45,7 @@ layui.define(['layer', 'form','base64'], function(exports){
 		return str;
 	}
 	
-	$(".view_kami").on("click","query-table tbody",function(event){
+	$("#query-table").on("click",".view_kami",function(event){
 		event.preventDefault();
 		var orderid = $(this).attr("data-orderid");
 		$(this).attr({"disabled":"disabled"});
