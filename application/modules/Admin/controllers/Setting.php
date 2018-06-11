b2e5b84db2d8b5b9936bdaba2a06ea54acae5d86
莫正强 <43036456@qq.com>
2018-06-11 17:25:59 +0800

    update

---
+++ application/modules/Admin/controllers/Setting.php
@@ -0,0 +1,29 @@
+<?php
+
+/*
+ * 功能：后台中心－首页
+ * author:资料空白
+ * time:20180509
+ */
+
+class SettingController extends AdminBasicController
+{
+
+    public function init()
+    {
+        parent::init();
+    }
+
+    public function indexAction()
+    {
+        if ($this->AdminUser==FALSE AND empty($this->AdminUser)) {
+            $this->redirect("/admin/login");
+            return FALSE;
+        }
+
+		$data = array();
+		$this->getView()->assign($data);
+    }
+
+
+}
