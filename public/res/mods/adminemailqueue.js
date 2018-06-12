cf0cac82b7fdcae009ad9661a59e52e02a7edba5
莫正强 <43036456@qq.com>
2018-06-12 17:57:00 +0800

    update

---
+++ public/res/mods/adminemailqueue.js
@@ -0,0 +1,23 @@
+layui.define(['layer', 'table'], function(exports){
+	var $ = layui.jquery;
+	var layer = layui.layer;
+	var table = layui.table;
+
+
+	table.render({
+		elem: '#emailqueue',
+		url: '/admin/emailqueue/ajax',
+		page: true,
+		cellMinWidth:60,
+		cols: [[
+			{field: 'id', title: 'ID', width:80},
+			{field: 'email', title: '收件人', minWidth:160},
+			{field: 'subject', title: '主题', minWidth:160},
+			{field: 'addtime', title: '添加时间', width:200, templet: '#addtime',align:'center'},
+			{field: 'sendtime', title: '发送时间', width:200, templet: '#sendtime',align:'center'}
+		]]
+	});
+
+
+	exports('adminemailqueue',null)
+});
