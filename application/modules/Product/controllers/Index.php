d30cad2903f3c216f12b5792e5183779ba4fd77f
莫正强 <43036456@qq.com>
2018-06-13 11:30:58 +0800

    update

--- application/modules/Product/controllers/Index.php
+++ application/modules/Product/controllers/Index.php
@@ -19,7 +19,7 @@ class IndexController extends PcBasicController
     {
 		$data = array();
 		$order = array('sort_num' => 'ASC');
-		$products_type = $this->m_products_type->Order($order)->Select();
+		$products_type = $this->m_products_type->Where(array('active'=>1))->Order($order)->Select();
 		$data['products_type'] = $products_type;
         $this->getView()->assign($data);
     }
