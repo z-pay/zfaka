c0d75b173243614dc17a328ca8d4bc9c4c154d9b
莫正强 <43036456@qq.com>
2018-06-13 16:24:51 +0800

    update

--- public/res/mods/adminproductscard.js
+++ public/res/mods/adminproductscard.js
@@ -4,19 +4,55 @@ layui.define(['layer', 'table', 'form','upload'], function(exports){
 	var table = layui.table;
 	var form = layui.form;
 	var upload = layui.upload;
-		//拖拽上传
-		upload.render({
-			elem: '#import_cards'
-			,url: '/admin/productscard/importajax/'
-			,auto: false
-			,accept: 'file' //普通文件
-			,exts: 'txt' //只允许txt文件
-			,size: 100 //限制文件大小，单位 KB
-			,bindAction: '#startUpload'
-			,done: function(res){
-				console.log(res)
+	//拖拽上传
+	upload.render({
+		elem: '#import_cards'
+		,url: '/admin/productscard/importajax/'
+		,auto: false
+		,accept: 'file' //普通文件
+		,exts: 'txt' //只允许txt文件
+		,size: 100 //限制文件大小，单位 KB
+		,bindAction: '#startUpload'
+		,done: function(res){
+			console.log(res)
+		}
+	});
+	//修改
+	form.on('submit(import)', function(data){
+		data.field.csrf_token = TOKEN;
+		var i = layer.load(2,{shade: [0.5,'#fff']});
+		$.ajax({
+			url: '/admin/productscard/importajax',
+			type: 'POST',
+			dataType: 'json',
+			data: data.field,
+		})
+		.done(function(res) {
+			if (res.code == '1') {
+				layer.open({
+					title: '提示',
+					content: '导入成功',
+					btn: ['确定'],
+					yes: function(index, layero){
+					    location.reload();
+					},
+					cancel: function(){ 
+					    location.reload();
+					}
+				});
+			} else {
+				layer.msg(res.msg,{icon:2,time:5000});
 			}
+		})
+		.fail(function() {
+			layer.msg('服务器连接失败，请联系管理员',{icon:2,time:5000});
+		})
+		.always(function() {
+			layer.close(i);
 		});
+
+		return false; //阻止表单跳转。如果需要表单跳转，去掉这段即可。
+	});
 	//删除
 	$(".productscard-table").on("click",".delete",function(event){
 		event.preventDefault();
