4f77a31fef154fa7226b3348fc6fbb6200017698
莫正强 <43036456@qq.com>
2018-06-13 10:40:45 +0800

    update

--- public/res/mods/adminpayment.js
+++ public/res/mods/adminpayment.js
@@ -14,10 +14,47 @@ layui.define(['layer', 'table'], function(exports){
 			{field: 'payment', title: '支付渠道', minWidth:160},
 			{field: 'alias', title: '别名', minWidth:160},
 			{field: 'app_id', title: 'APPID'},
-			{field: 'active', title: '是否激活', width:200, templet: '#active',align:'center'}
+			{field: 'active', title: '是否激活', width:200, templet: '#active',align:'center'},
+			{field: 'opt', title: '操作', width:200, templet: '#opt',align:'center'}
 		]]
 	});
 
+	//修改
+	form.on('submit(edit)', function(data){
 
+		data.field.csrf_token = TOKEN;
+		var i = layer.load(2,{shade: [0.5,'#fff']});
+		$.ajax({
+			url: '/admin/payment/editajax',
+			type: 'POST',
+			dataType: 'json',
+			data: data.field,
+		})
+		.done(function(res) {
+			if (res.code == '1') {
+				layer.open({
+					title: '提示',
+					content: '修改成功',
+					btn: ['确定'],
+					yes: function(index, layero){
+					    location.reload();
+					},
+					cancel: function(){ 
+					    location.reload();
+					}
+				});
+			} else {
+				layer.msg(res.msg,{icon:2,time:5000});
+			}
+		})
+		.fail(function() {
+			layer.msg('服务器连接失败，请联系管理员',{icon:2,time:5000});
+		})
+		.always(function() {
+			layer.close(i);
+		});
+
+		return false; //阻止表单跳转。如果需要表单跳转，去掉这段即可。
+	});
 	exports('adminpayment',null)
 });
