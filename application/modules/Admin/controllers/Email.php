5d39dd42aa331fc90d7291747ec612363ef32290
莫正强 <43036456@qq.com>
2018-06-13 09:31:01 +0800

    update

---
+++ application/modules/Admin/controllers/Email.php
@@ -0,0 +1,58 @@
+<?php
+
+/*
+ * 功能：后台中心－邮箱设置
+ * Author:资料空白
+ * Date:20180509
+ */
+
+class EmailController extends PcBasicController
+{
+    private $m_email;
+	
+	public function init()
+    {
+        parent::init();
+		$this->m_email = $this->load('email');
+    }
+
+    public function indexAction()
+    {
+        if ($this->AdminUser==FALSE AND empty($this->AdminUser)) {
+            $this->redirect("/admin/login");
+            return FALSE;
+        }
+		$data = array();
+		$email = $this->m_email->SelectOne();
+		$data['email'] = $email;
+        $this->getView()->assign($data);
+    }
+
+	public function ajaxAction()
+	{
+		$nickname = $this->getPost('nickname',false);
+		$qq = $this->getPost('qq',false);
+		$tag = $this->getPost('tag',false);
+		$csrf_token = $this->getPost('csrf_token', false);
+		
+		$data = array();
+		
+        if ($this->login==FALSE AND !$this->userid) {
+            $data = array('code' => 1000, 'msg' => '请登录');
+			Helper::response($data);
+        }
+		
+		if($nickname AND $csrf_token){
+			if ($this->VerifyCsrfToken($csrf_token)) {
+				$this->m_user->UpdateByID(array('nickname'=>$nickname,'qq'=>$qq,'tag'=>$tag),$this->userid);
+				$data = array('code' => 1, 'msg' => '更新成功');
+			} else {
+                $data = array('code' => 1001, 'msg' => '页面超时，请刷新页面后重试!');
+            }
+		}else{
+			$data = array('code' => 1000, 'msg' => '丢失参数');
+		}
+		Helper::response($data);
+	}
+
+}
---
+++ public/res/mods/adminemail.js
@@ -0,0 +1,46 @@
+layui.define(['layer', 'form'], function(exports){
+	var $ = layui.jquery;
+	var layer = layui.layer;
+	var form = layui.form;
+
+
+	//修改资料
+	form.on('submit(email)', function(data){
+
+		data.field.csrf_token = TOKEN;
+		var i = layer.load(2,{shade: [0.5,'#fff']});
+		$.ajax({
+			url: '/admin/email/ajax',
+			type: 'POST',
+			dataType: 'json',
+			data: data.field,
+		})
+		.done(function(res) {
+			if (res.code == '1') {
+				layer.open({
+					title: '提示',
+					content: '邮箱修改成功',
+					btn: ['确定'],
+					yes: function(index, layero){
+					    location.reload();
+					},
+					cancel: function(){ 
+					    location.reload();
+					}
+				});
+			} else {
+				layer.msg(res.msg,{icon:2,time:5000});
+			}
+		})
+		.fail(function() {
+			layer.msg('服务器连接失败，请联系管理员',{icon:2,time:5000});
+		})
+		.always(function() {
+			layer.close(i);
+		});
+
+		return false; //阻止表单跳转。如果需要表单跳转，去掉这段即可。
+	});
+
+	exports('adminemail',null)
+});
