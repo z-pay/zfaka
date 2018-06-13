c640f725e7a9527baee2a69c07e6f4f1e3b6f8a9
莫正强 <43036456@qq.com>
2018-06-13 08:51:49 +0800

    update

--- application/model/M_User_group.php
+++ application/model/M_User_group.php
@@ -13,4 +13,33 @@ class M_User_group extends Model
 		$this->table = TB_PREFIX.'user_group';
 		parent::__construct();
 	}
+	
+	public function getConfig($new=0){
+		$data = $config = array();
+		$file_path=TEMP_PATH ."/user_group.json";
+		if(file_exists($file_path) AND !$new){
+			$data = json_decode(file_get_contents($file_path),true);
+		}
+	
+		//取旧值
+		if(!empty($data) AND isset($data['config']) AND (isset($data['expire_time']) AND $data['expire_time'] > time())){
+			$config =$data['config'];
+		}
+		if (empty($config) OR $new){
+    		$data['config'] = $config = $this->_getData();
+    		$data['expire_time'] = time() + 600;
+    		
+			file_put_contents($file_path,json_encode($data));
+    	}
+		
+		return $config;
+	} 
+
+	private function _getData(){
+		$result=$this->Select();
+		foreach($result AS $i){
+			$config[$i['id']]=htmlspecialchars_decode($i['name'],ENT_QUOTES);
+		}
+		return $config;
+	}	
 }
