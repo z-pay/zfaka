05a8bd110caf1373531520d8d7b13cc58dfd1cbe
莫正强 <43036456@qq.com>
2018-06-27 11:43:30 +0800

    update

--- application/modules/Install/controllers/Setpone.php
+++ application/modules/Install/controllers/Setpone.php
@@ -21,9 +21,25 @@ class SetponeController extends BasicController
 			return FALSE;
 		}else{
 			$data = array();
+			$require = array(
+				array('name' => 'PHP版本','require'=>'7.0.0','result'=>$this->_isVersion('7.0.0',phpversion())),
+				array('name' => 'Curl支持','require'=>'必须','result'=>$this->_isfun('curl_init')),
+				
+			);
+			
+			$data['require'] = $require;
 			$this->getView()->assign($data);
 		}
     }
 	
-
+	private function _isfun($funName = '')
+	{
+		if (!$funName || trim($funName) == '' || preg_match('~[^a-z0-9\_]+~i', $funName, $tmp)) return '错误';
+		return (false !== function_exists($funName)) ? '<font color="green">√</font>' : '<font color="red">×</font>';
+	}
+	
+	private function _isVersion($version = '',$required_version = '')
+	{
+		return (false !== version_compare( $version, $required_version, '>=' )) ? '<font color="green">√</font>' : '<font color="red">×</font>';
+	}
 }
