d0f91f8bda433eacdba8fdd33dd1482ada231e1b
莫正强 <43036456@qq.com>
2018-06-29 15:05:59 +0800

    修复class未定义bug

--- application/modules/Product/controllers/Notify.php
+++ application/modules/Product/controllers/Notify.php
@@ -25,7 +25,8 @@ class NotifyController extends PcBasicController
 			if(isset($payments[$paymethod]) AND !empty($payments[$paymethod])){
 				try {
 					$payconfig = $payments[$paymethod];
-					$PAY = "\\Pay\\".$paymethod."\\".$paymethod;
+					$payclass = "\\Pay\\".$paymethod."\\".$paymethod;
+					$PAY = new $payclass();
 					$data = $PAY->notify($payconfig,$_POST);
 					if($data['code']=='1'){
 						echo 'success';exit();
