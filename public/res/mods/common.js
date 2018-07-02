7240ddc571a9e1c79fbd11b1a4cf484d4d85ba37
莫正强 <43036456@qq.com>
2018-07-02 10:14:32 +0800

    update

--- public/res/mods/common.js
+++ public/res/mods/common.js
@@ -1,6 +1,6 @@
 ﻿/**
 
- @Name: Fly社区主入口
+ @Name: ZFAKA平台框架
 
  */
 
@@ -19,7 +19,7 @@ layui.define(['layer', 'laytpl', 'form', 'element', 'util'], function(exports){
 
   //阻止IE7以下访问
   if(device.ie && device.ie < 8){
-    layer.alert('如果您非得使用 IE 浏览器访问Fly社区，那么请使用 IE8+');
+    layer.alert('如果您非得使用 IE 浏览器访问本站点，那么请使用 IE8+');
   }
 
   layui.focusInsert = function(obj, str){
@@ -61,13 +61,12 @@ layui.define(['layer', 'laytpl', 'form', 'element', 'util'], function(exports){
 
 
   //加载特定模块
-  if(layui.cache.page && layui.cache.page !== 'index'){
+  /*if(layui.cache.page && layui.cache.page !== 'index'){
     var extend = {};
     extend[layui.cache.page] = layui.cache.page;
     layui.extend(extend);
     layui.use(layui.cache.page);
-  }
-
+  }*/
 
 
 
@@ -83,7 +82,7 @@ layui.define(['layer', 'laytpl', 'form', 'element', 'util'], function(exports){
     $('body').removeClass('site-mobile');
   });
 
-  exports('fly',null);
+  exports('common',null);
 
 });
 
