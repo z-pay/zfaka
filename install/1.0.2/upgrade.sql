5ec656490d8e20b7404c5b05cae687a904919d55
莫正强 <43036456@qq.com>
2018-07-02 15:42:39 +0800

    修复初始化支付图片路径

--- install/1.0.2/upgrade.sql
+++ install/1.0.2/upgrade.sql
@@ -1,2 +1,2 @@
 INSERT INTO `t_payment` (`id`, `payment`, `payname`, `payimage`, `alias`, `sign_type`, `app_id`, `app_secret`, `ali_public_key`, `rsa_private_key`, `notify_url`, `return_url`, `active`) VALUES
-(3, 'QQ扫码支付', '手机QQ', '/res/images/pay/qq.jpg', 'codepayqq', '', '', '', '', '', '/product/notify/', '/product/query/', 0);
+(3, 'QQ扫码支付', '手机QQ', '/res/images/pay/qqpay.jpg', 'codepayqq', '', '', '', '', '', '/product/notify/', '/product/query/', 0);
