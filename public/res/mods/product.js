1a27a70d9efa3995becf1e7471441020a1f4cd5c
zlkb <43036456@qq.com>
2018-07-04 21:07:06 +0800

    改造

--- public/res/mods/product.js
+++ public/res/mods/product.js
@@ -4,10 +4,10 @@
 	var form = layui.form;
 
     function htmlspecialchars_decode(str){
+		str = str.replace(/&amp;/g, '&');
         str = str.replace(/&lt;/g, '<');
 		str = str.replace(/&gt;/g, '>');
 		str = str.replace(/&quot;/g, '"');
-		str = str.replace(/&amp;/g, '&');
         str = str.replace(/&#039;/g, "'");
         return str;  
     }
