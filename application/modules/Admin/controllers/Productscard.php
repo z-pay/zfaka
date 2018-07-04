c5c54d428a8513555fd92e038a39b95f1f13eced
莫正强 <43036456@qq.com>
2018-07-04 11:40:23 +0800

    完善

--- application/modules/Admin/controllers/Productscard.php
+++ application/modules/Admin/controllers/Productscard.php
@@ -66,7 +66,7 @@ class ProductscardController extends AdminBasicController
             }
 			
             $limits = "{$pagenum},{$limit}";
-			$sql ="SELECT p1.*,p2.name FROM `t_products_card` as p1 left join `t_products` as p2 on p1.pid=p2.id Where p1.isdelete=0 {$where1} Order by p1.id desc LIMIT {$limits}";
+			$sql ="SELECT p1.*,p2.name FROM `t_products_card` as p1 left join `t_products` as p2 on p1.pid=p2.id Where p1.isdelete=0 and {$where1} Order by p1.id desc LIMIT {$limits}";
 			$items=$this->m_products_card->Query($sql);
 			
             if (empty($items)) {
