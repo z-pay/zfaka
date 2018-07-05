7d5447bd25c9ee3e403d362132da02dbb716ef2c
莫正强 <43036456@qq.com>
2018-07-05 11:55:52 +0800

    改造

--- public/res/mods/productpay.js
+++ public/res/mods/productpay.js
@@ -16,9 +16,9 @@
             success: function(res) {
                 if (res.code == 1) {
 					if(res.data.overtime>0){
-						timer(res.data.overtime);
+						timer(res.data.overtime,paymethod);
 						var html = '<h1 class="mod-title"><span class="ico_log ico-'+paymethod+'"></span></h1><div class="mod-content" style="text-align: center;"><img id="pay_qrcode" src="'+res.data.qr+'" alt="'+res.data.payname+'" width="230" height="230">';
-						html +='<div id="time-item" class="time-item"><strong id="hour_show"><s id="h"></s>0时</strong><strong id="minute_show"><s></s>05分</strong><strong id="second_show"><s></s>08秒</strong>';
+						html +='<div id="time-item" class="time-item"><strong id="hour_show_'+paymethod+'"><s id="h"></s>0时</strong><strong id="minute_show_'+paymethod+'"><s></s>05分</strong><strong id="second_show_'+paymethod+'"><s></s>08秒</strong>';
 						html +='<p>请使用手机'+res.data.payname+'扫一扫</p><p>扫描二维码完成支付</p></div></div>';
 					}else{
 						var html = '<h1 class="mod-title"><span class="ico_log ico-'+paymethod+'"></span></h1><div class="mod-content" style="text-align: center;"><img id="pay_qrcode" src="'+res.data.qr+'" alt="'+res.data.payname+'" width="230" height="230">';
@@ -76,7 +76,7 @@
 		//return true;
     }
 	
-	function timer(intDiff) {
+	function timer(intDiff,paymethod) {
 		var i = 0;
 		myTimer = window.setInterval(function () {
 			i++;
@@ -92,9 +92,9 @@
 			}
 			if (minute <= 9) minute = '0' + minute;
 			if (second <= 9) second = '0' + second;
-			$('#hour_show').html('<s id="h"></s>' + hour + '时');
-			$('#minute_show').html('<s></s>' + minute + '分');
-			$('#second_show').html('<s></s>' + second + '秒');
+			$('#hour_show_'+paymethod).html('<s id="h"></s>' + hour + '时');
+			$('#minute_show_'+paymethod).html('<s></s>' + minute + '分');
+			$('#second_show_'+paymethod).html('<s></s>' + second + '秒');
 			if (hour <= 0 && minute <= 0 && second <= 0) {
 				$("#pay_qrcode").attr("src", '/res/images/pay/overtime.jpg');
 				$("#pay_qrcode").attr("alt", '二维码失效');
