d63fb897d909e882b1fbd67dbcf7d37083131c39
zlkb <43036456@qq.com>
2018-07-07 20:49:57 +0800

    改造

--- public/res/mods/product.js
+++ public/res/mods/product.js
@@ -167,8 +167,8 @@
 	}
 
 	//对商品描述再做一次补充解密处理
-	html = htmlspecialchars_decode($('#prodcut_description').text());
-	$('#prodcut_description').html(html);
+	//html = htmlspecialchars_decode($('#prodcut_description').text());
+	//$('#prodcut_description').html(html);
 	
 	autoHeight();
 	exports('product',null)
