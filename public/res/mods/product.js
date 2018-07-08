455e784c5e647d634ece75eaed2c451341714ad6
zlkb <43036456@qq.com>
2018-07-08 14:06:24 +0800

    修复htmlspecialchars_decode问题

--- public/res/mods/product.js
+++ public/res/mods/product.js
@@ -167,11 +167,11 @@
 	}
 
 	//对商品描述再做一次补充解密处理
-	var aName = window.location.pathname;
+	/*var aName = window.location.pathname;
 	if (aName.indexOf('/product/detail') >-1) {
 		html = htmlspecialchars_decode($('#prodcut_description').text());
 		$('#prodcut_description').html(html);
-	}
+	}*/
 	autoHeight();
 	exports('product',null)
 });
