dc944eb5bf940300678edec7145ce5d1d499f16f
zlkb <43036456@qq.com>
2018-07-10 21:13:11 +0800

    改造

--- public/res/mods/common.js
+++ public/res/mods/common.js
@@ -101,7 +101,8 @@ layui.define(['layer', 'laytpl', 'form', 'element','table'], function(exports){
 			$('#main-menu-mobile').hide();
 		}
 	});
-	$('.main-menu-mobile_body .site-mobile-shade').on('click', function(){
+	$('.site-mobile-shade').on('click', function(){
+		$('body').removeClass('main-menu-mobile_body');
 		$('#main-menu-mobile').hide();
 	});
  	//全局删除信息提示
