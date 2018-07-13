c4de0f0e3d708dd926ffc5142ee6485b5e902738
莫正强 <43036456@qq.com>
2018-07-13 08:04:14 +0800

    准备开始分离后台

--- application/modules/Admin/controllers/Email.php
+++ application/modules/Admin/controllers/Email.php
@@ -19,7 +19,7 @@ class EmailController extends AdminBasicController
     public function indexAction()
     {
         if ($this->AdminUser==FALSE AND empty($this->AdminUser)) {
-            $this->redirect("/admin/login");
+            $this->redirect(ADMIN_DIR."/login");
             return FALSE;
         }
 		$data = array();
