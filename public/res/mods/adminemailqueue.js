c37976f7038ed5c3701d93c3145527a6632fd864
莫正强 <43036456@qq.com>
2018-09-03 11:24:46 +0800

    修复bug

--- public/res/mods/adminemailqueue.js
+++ public/res/mods/adminemailqueue.js
@@ -14,7 +14,7 @@ layui.define(['layer', 'table'], function(exports){
 			{field: 'email', title: '收件人', minWidth:160},
 			{field: 'subject', title: '主题', minWidth:160},
 			{field: 'addtime', title: '添加时间', width:200, templet: '#addtime',align:'center'},
-			{field: 'sendtime', title: '发送时间', width:200, templet: '#sendtime',align:'center'}
+			{field: 'sendtime', title: '发送时间', width:200, templet: '#sendtime',align:'center'},
 			{field: 'status', title: '状态', width:80, templet: '#status',align:'center'},
 			{field: 'opt', title: '操作', templet: '#opt',align:'center',fixed: 'right', width: 160},
 		]]
