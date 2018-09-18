5f95abb43501c10838fa9d09b57d09fc9eed78a2
莫正强 <43036456@qq.com>
2018-09-18 10:27:47 +0800

    再次优化首页

--- public/res/mods/product-list.js
+++ public/res/mods/product-list.js
@@ -1,22 +1,35 @@
-layui.define(['layer', 'table'], function(exports){
+layui.define(['layer', 'table','device'], function(exports){
 	var $ = layui.jquery;
 	var layer = layui.layer;
 	var table = layui.table;
+	var device = layui.device();
 	
-
-	table.render({
-		elem: '#table',
-		url: '/product/get',
-		page: true,
-		cellMinWidth:60,
-		cols: [[
-			{field: 'name', title: '商品名称',minWidth:120},
-			{field: 'price', title: '单价', width:80,},
-			{field: 'qty', title: '库存', width:80, templet: '#qty',align:'center'},
-			{field: 'auto', title: '发货模式', width:100, templet: '#auto',align:'center'},
-			{field: 'opt', title: '操作', width:120, templet: '#opt',align:'center',fixed: 'right'},
-		]]
-	});
-
+	if(device.weixin || device.android || device.ios){
+		table.render({
+			elem: '#table',
+			url: '/product/get',
+			page: true,
+			cellMinWidth:60,
+			cols: [[
+				{field: 'name', title: '商品名称',minWidth:120},
+				{field: 'price', title: '单价', width:80,},
+				{field: 'opt', title: '操作', width:80, templet: '#opt',align:'center',fixed: 'right'},
+			]]
+		});
+	}else{
+		table.render({
+			elem: '#table',
+			url: '/product/get',
+			page: true,
+			cellMinWidth:60,
+			cols: [[
+				{field: 'name', title: '商品名称',minWidth:120},
+				{field: 'price', title: '单价', width:80,},
+				{field: 'qty', title: '库存', width:80, templet: '#qty',align:'center'},
+				{field: 'auto', title: '发货模式', width:100, templet: '#auto',align:'center'},
+				{field: 'opt', title: '操作', width:120, templet: '#opt',align:'center',fixed: 'right'},
+			]]
+		});
+	}
 	exports('product-list',null)
 });
