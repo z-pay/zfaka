87d2e801661b4c889bc169c10e05a7b078612b1f
莫正强 <43036456@qq.com>
2018-09-18 09:52:31 +0800

    完善输出

--- public/res/mods/product-list.js
+++ public/res/mods/product-list.js
@@ -10,9 +10,8 @@ layui.define(['layer', 'table'], function(exports){
 		page: true,
 		cellMinWidth:60,
 		cols: [[
-			{field: 'id', title: 'ID', width:80},
 			{field: 'name', title: '商品名称'},
-			{field: 'price', title: '单价'},
+			{field: 'price', title: '单价', width:80,},
 			{field: 'qty', title: '库存', width:80, templet: '#qty',align:'center'},
 			{field: 'auto', title: '发货模式', width:100, templet: '#auto',align:'center'},
 			{field: 'opt', title: '操作', width:160, templet: '#opt',align:'center'},
