9bc8fe5fe814eaa7e717d2329705ffc9cbee9a01
莫正强 <43036456@qq.com>
2018-09-18 10:21:46 +0800

    美化首页

--- public/res/mods/product-list.js
+++ public/res/mods/product-list.js
@@ -10,11 +10,11 @@ layui.define(['layer', 'table'], function(exports){
 		page: true,
 		cellMinWidth:60,
 		cols: [[
-			{field: 'name', title: '商品名称'},
+			{field: 'name', title: '商品名称',minWidth:120},
 			{field: 'price', title: '单价', width:80,},
 			{field: 'qty', title: '库存', width:80, templet: '#qty',align:'center'},
 			{field: 'auto', title: '发货模式', width:100, templet: '#auto',align:'center'},
-			{field: 'opt', title: '操作', width:160, templet: '#opt',align:'center'},
+			{field: 'opt', title: '操作', width:120, templet: '#opt',align:'center',fixed: 'right'},
 		]]
 	});
 
