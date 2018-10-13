71fcbe14010c9f37ce4a7b219795166b31467be7
莫正强 <43036456@qq.com>
2018-10-13 18:34:58 +0800

    修复发卡bug

--- application/library/Pay/notify.php
+++ application/library/Pay/notify.php
@@ -44,7 +44,12 @@ class notify
 						if($product['auto']>0){
 							//3.自动处理
 							//查询通过订单中记录的pid，根据购买数量查询卡密
-							$cards = $m_products_card->Where(array('pid'=>$order['pid'],'active'=>0,'isdelete'=>0))->Limit($order['number'])->Select();
+							if($product['stockcontrol']>0){
+								$Limit = $order['number'];
+							}else{
+								$Limit = 1;
+							}
+							$cards = $m_products_card->Where(array('pid'=>$order['pid'],'active'=>0,'isdelete'=>0))->Limit($Limit)->Select();
 							if(is_array($cards) AND !empty($cards) AND count($cards)==$order['number']){
 								//3.1 库存充足,获取对应的卡id,卡密
 								$card_mi_array = array_column($cards, 'card');
