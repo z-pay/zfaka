951710b3c1873deb1b5dae482720ec53971b5390
莫正强 <43036456@qq.com>
2018-10-29 15:31:47 +0800

    更新代码

--- install/1.2.1/upgrade.sql
+++ install/1.2.1/upgrade.sql
@@ -10,6 +10,7 @@ UPDATE `t_config` SET `name` = 'orderinputtype' WHERE `id` = 11;
 UPDATE `t_config` SET `name` = 'tplindex' WHERE `id` = 12;
 UPDATE `t_config` SET `name` = 'mprodcutdescriptionswitch' WHERE `id` = 15;
 UPDATE `t_config` SET `name` = 'orderprefix' WHERE `id` = 16;
+UPDATE `t_payment` SET `payment` = '收款宝' WHERE `id` = 8;
 INSERT INTO `t_config` (`id`, `catid`, `name`, `value`, `tag`, `lock`, `updatetime`) VALUES
 (21, 1, 'loginswitch', '1', '登录开关', 1, 1453452674),
 (22, 1, 'forgetpwdswitch', '0', '找回密码开关', 1, 1453452674);
