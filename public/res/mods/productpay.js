4a201e80f45f8cac2b4cda2b4c4ffc2d2adff027
莫正强 <43036456@qq.com>
2018-10-30 09:36:03 +0800

    增加自动跳转

--- public/res/mods/productpay.js
+++ public/res/mods/productpay.js
@@ -57,6 +57,11 @@
 							   clearInterval(myTimer);
 							} 
 						  });
+						  //3秒后自动跳转
+							setTimeout(function(){
+								location.href = res.data.url;
+							},3000);
+						  
 					}else{
 						if(res.data.overtime>0){
 							timer(res.data.overtime,paymethod);
