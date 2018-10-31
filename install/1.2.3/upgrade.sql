94b8e44f2987f16ad4d4ba47fe4d1e72ea7fbb48
莫正强 <43036456@qq.com>
2018-10-31 13:20:00 +0800

    添加分类密码尝试

---
+++ install/1.2.3/upgrade.sql
@@ -0,0 +1,1 @@
+ALTER TABLE `t_products_type` ADD `password` VARCHAR(60) NOT NULL COMMENT '分类密码' AFTER `name`;
--- public/res/mods/product.js
+++ public/res/mods/product.js
@@ -52,36 +52,53 @@
 	
 	form.on('select(typeid)', function(data){
 		if (data.value == 0) return;
-		$.ajax({
-			url: '/product/get/proudctlist',
-			type: 'POST',
-			dataType: 'json',
-			data: {'tid': data.value,'csrf_token':TOKEN},
-			beforeSend: function () {
-			},
-			success: function (res) {
-				if (res.code == '1') {
-					var html = "";
-					var list = res.data.products;
-					for (var i = 0, j = list.length; i < j; i++) {
-						html += '<option value='+list[i].id+'>'+list[i].name+'</option>';
+		var ispassword = $(this).attr("data-type");
+		if(ispassword>0){
+			//示范一个公告层
+			  layer.open({
+				type: 1
+				,title: false //不显示标题栏
+				,closeBtn: false
+				,area: '300px;'
+				,shade: 0.8
+				,id: 'LAY_layuipro' //设定一个id，防止重复弹出
+				,btn: ['放弃']
+				,btnAlign: 'c'
+				,moveType: 1 //拖拽模式，0或者1
+				,content: '<div style="padding: 50px; line-height: 22px; background-color: #393D49; color: #fff; font-weight: 300;">请输入密码</div>'
+			  });
+		}else{
+			$.ajax({
+				url: '/product/get/proudctlist',
+				type: 'POST',
+				dataType: 'json',
+				data: {'tid': data.value,'csrf_token':TOKEN},
+				beforeSend: function () {
+				},
+				success: function (res) {
+					if (res.code == '1') {
+						var html = "";
+						var list = res.data.products;
+						for (var i = 0, j = list.length; i < j; i++) {
+							html += '<option value='+list[i].id+'>'+list[i].name+'</option>';
+						}
+						$('#productlist').html("<option value=\"0\">请选择</option>" + html);
+						$('#price').val('');
+						$('#qty').val('');
+						$('#prodcut_description').html('');
+						$("#buy").attr("disabled","true");
+						$("#addons").remove();
+						form.render('select');
+						autoHeight();
+					} else {
+						$("#buy").attr("disabled","true");
+						form.render('select');
+						layer.msg(res.msg,{icon:2,time:5000});
 					}
-					$('#productlist').html("<option value=\"0\">请选择</option>" + html);
-					$('#price').val('');
-					$('#qty').val('');
-					$('#prodcut_description').html('');
-					$("#buy").attr("disabled","true");
-					$("#addons").remove();
-					form.render('select');
-					autoHeight();
-				} else {
-					$("#buy").attr("disabled","true");
-					form.render('select');
-					layer.msg(res.msg,{icon:2,time:5000});
 				}
-			}
 
-		});
+			});
+		}
 	});
 
 	form.on('select(productlist)', function(data){
