7112b1b5898a11ab550154e9f0881dfcc71c7145
莫正强 <43036456@qq.com>
2018-10-31 17:40:46 +0800

    修复

--- public/res/mods/product.js
+++ public/res/mods/product.js
@@ -50,40 +50,6 @@
 		return true;
 	}
 	
-	function getproudctlist(data){
-		//远程请求验证
-		$.ajax({
-			url: '/product/get/proudctlist',
-			type: 'POST',
-			dataType: 'json',
-			data: {'tid': data.value,'password':grouppassword,'csrf_token':TOKEN},
-			beforeSend: function () {
-			},
-			success: function (res) {
-				if (res.code == '1') {
-					var html = "";
-					var list = res.data.products;
-					for (var i = 0, j = list.length; i < j; i++) {
-						html += '<option value='+list[i].id+'>'+list[i].name+'</option>';
-					}
-					$('#productlist').html("<option value=\"0\">请选择</option>" + html);
-					$('#price').val('');
-					$('#qty').val('');
-					$('#prodcut_description').html('');
-					$("#buy").attr("disabled","true");
-					$("#addons").remove();
-					form.render('select');
-					autoHeight();
-				} else {
-					$("#buy").attr("disabled","true");
-					form.render('select');
-					layer.msg(res.msg,{icon:2,time:5000});
-				}
-			},
-
-		});
-	}
-	
 	form.on('select(typeid)', function(data){
 		if (data.value == 0) return;
 		var ispassword = $(data.elem).find('option:selected').data('type');
@@ -103,7 +69,37 @@
 				,yes: function(layero){
 					var grouppassword = $("#grouppassword").val(); 
 					if(grouppassword.length>0){
-						getproudctlist(data);
+						//远程请求验证
+						$.ajax({
+							url: '/product/get/proudctlist',
+							type: 'POST',
+							dataType: 'json',
+							data: {'tid': data.value,'password':grouppassword,'csrf_token':TOKEN},
+							beforeSend: function () {
+							},
+							success: function (res) {
+								if (res.code == '1') {
+									var html = "";
+									var list = res.data.products;
+									for (var i = 0, j = list.length; i < j; i++) {
+										html += '<option value='+list[i].id+'>'+list[i].name+'</option>';
+									}
+									$('#productlist').html("<option value=\"0\">请选择</option>" + html);
+									$('#price').val('');
+									$('#qty').val('');
+									$('#prodcut_description').html('');
+									$("#buy").attr("disabled","true");
+									$("#addons").remove();
+									form.render('select');
+									autoHeight();
+								} else {
+									$("#buy").attr("disabled","true");
+									form.render('select');
+									layer.msg(res.msg,{icon:2,time:5000});
+								}
+							},
+
+						});
 						layer.closeAll();
 					}else{
 						layer.msg("请输入密码",{icon:2,time:5000});
@@ -119,7 +115,37 @@
 				}
 			});
 		}else{
-			getproudctlist(data);
+			//远程请求验证
+			$.ajax({
+				url: '/product/get/proudctlist',
+				type: 'POST',
+				dataType: 'json',
+				data: {'tid': data.value,'password':grouppassword,'csrf_token':TOKEN},
+				beforeSend: function () {
+				},
+				success: function (res) {
+					if (res.code == '1') {
+						var html = "";
+						var list = res.data.products;
+						for (var i = 0, j = list.length; i < j; i++) {
+							html += '<option value='+list[i].id+'>'+list[i].name+'</option>';
+						}
+						$('#productlist').html("<option value=\"0\">请选择</option>" + html);
+						$('#price').val('');
+						$('#qty').val('');
+						$('#prodcut_description').html('');
+						$("#buy").attr("disabled","true");
+						$("#addons").remove();
+						form.render('select');
+						autoHeight();
+					} else {
+						$("#buy").attr("disabled","true");
+						form.render('select');
+						layer.msg(res.msg,{icon:2,time:5000});
+					}
+				},
+
+			});
 		}
 	});
 
