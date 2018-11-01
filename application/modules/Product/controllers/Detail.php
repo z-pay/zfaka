7b40a01951a4005b5b10f8f41ff49873e93e2b93
莫正强 <43036456@qq.com>
2018-11-01 11:43:56 +0800

    针对独立商品页进行调整

--- application/modules/Product/controllers/Detail.php
+++ application/modules/Product/controllers/Detail.php
@@ -21,17 +21,30 @@ class DetailController extends PcBasicController
 		if($pid AND is_numeric($pid) AND $pid>0){
 			$product = $this->m_products->Where(array('id'=>$pid,'active'=>1,'isdelete'=>0))->SelectOne();
 			if(!empty($product)){
-				$data['product'] = $product;
-				
-				if($product['addons']){
-					$addons = explode(',',$product['addons']);
-					$data['addons'] = $addons;
+				$data = array();
+				//如果是密码商品
+				if(strlen($product['password'])>0){
+					$tpl = "t_password";
+					if(file_exists(APP_PATH.'/application/modules/Product/views/detail/tpl/'.$tpl.'.html')){
+						$data['title'] = $product['name']."_购买商品";
+						$this->display($tpl, $data);
+						return FALSE;
+					}else{
+						$this->redirect("/product/");
+						return FALSE;	
+					}
 				}else{
-					$data['addons'] = array();
+				//否则
+					$data['product'] = $product;
+					if($product['addons']){
+						$addons = explode(',',$product['addons']);
+						$data['addons'] = $addons;
+					}else{
+						$data['addons'] = array();
+					}
+					$data['title'] = $product['name']."_购买商品";
+					$this->getView()->assign($data);
 				}
-				
-				$data['title'] = $product['name']."_购买商品";
-				$this->getView()->assign($data);
 			}else{
 				$this->redirect("/product/");
 				return FALSE;	
