a1716b046cae4cfaba986617895736bf25c47897
莫正强 <43036456@qq.com>
2018-11-13 09:13:49 +0800

    新增配置项修复数据异常功能

--- application/modules/Admin/controllers/Setting.php
+++ application/modules/Admin/controllers/Setting.php
@@ -142,4 +142,40 @@ class SettingController extends AdminBasicController
 		}
 		Helper::response($data);
 	}	
+	
+	public function repairajaxAction()
+	{
+		$method = $this->getPost('method',false);
+		$csrf_token = $this->getPost('csrf_token', false);
+		
+		$data = array();
+		
+        if ($this->AdminUser==FALSE AND empty($this->AdminUser)) {
+            $data = array('code' => 1000, 'msg' => '请登录');
+			Helper::response($data);
+        }
+		
+		if($method AND $csrf_token){
+			if ($this->VerifyCsrfToken($csrf_token)) {
+				$field = array('id','name','updatetime','tag');
+				$items = $this->m_config->Field($field)->Order(array('id'=>'DESC'))->Select();
+				if (empty($items)) {
+					$data = array('code' => 1004, 'msg' => '无数据，不需要修复');
+				} else {
+					foreach($items AS $item){
+						$tag = getRawText($item['tag']);
+						$m = array('tag'=>htmlspecialchars($tag));
+						$this->m_config->UpdateByID($m,$item['id']);
+						unset($tag,$m);
+					}
+					$data = array('code' => 1, 'msg' => '修复完成');
+				}
+			} else {
+                $data = array('code' => 1001, 'msg' => '页面超时，请刷新页面后重试!');
+            }
+		}else{
+			$data = array('code' => 1000, 'msg' => '丢失参数');
+		}
+		Helper::response($data);
+	}		
 }
