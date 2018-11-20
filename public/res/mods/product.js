fa485db07ce5794f6b8ebb0c7ada088547c56ec0
莫正强 <43036456@qq.com>
2018-11-20 15:49:17 +0800

    美化

--- public/res/mods/product.js
+++ public/res/mods/product.js
@@ -15,7 +15,7 @@
 		var money = 0;
 		var price = parseFloat($('#price').val());
 		var qty = Number($('#number').val());
-		money = price*qty;
+		money = changeTwoDecimal_f(price*qty);
 		$('#money').val(money);
     });
 	
@@ -35,8 +35,26 @@
 				return '查询密码不能有特殊字符';
 			}
 		}
-	});	
+	});
 	
+	function changeTwoDecimal_f(x) {
+		var f_x = parseFloat(x);
+		if (isNaN(f_x)) {
+			alert('function:changeTwoDecimal->parameter error');
+			return false;
+		}
+		var f_x = Math.round(x * 100) / 100;
+		var s_x = f_x.toString();
+		var pos_decimal = s_x.indexOf('.');
+		if (pos_decimal < 0) {
+			pos_decimal = s_x.length;
+			s_x += '.';
+		}
+		while (s_x.length <= pos_decimal + 2) {
+			s_x += '0';
+		}
+		return s_x;
+	}	
     function htmlspecialchars_decode(str){
 		if(str.length>0){
 			str = str.replace(/&amp;/g, '&');
