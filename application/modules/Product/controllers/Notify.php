f310bca1235b48a08350ba8d38ac90a2c48b2547
莫正强 <43036456@qq.com>
2018-12-05 13:04:18 +0800

    支付回调修改为支持GET请求

--- application/modules/Product/controllers/Notify.php
+++ application/modules/Product/controllers/Notify.php
@@ -16,28 +16,22 @@ class NotifyController extends PcBasicController
 	
     public function indexAction()
     {
-		if($this->getRequest()->isPost()){
-			$paymethod = isset($_GET['paymethod'])?$_GET['paymethod']:(isset($_POST['paymethod'])?$_POST['paymethod']:'zfbf2f');
-			$payments = $this->m_payment->getConfig();
-			if(isset($payments[$paymethod]) AND !empty($payments[$paymethod])){
-				try {
-					$payconfig = $payments[$paymethod];
-					$payclass = "\\Pay\\".$paymethod."\\".$paymethod;
-					$PAY = new $payclass();
-					echo $result = $PAY->notify($payconfig);
-					file_put_contents(YEWU_FILE, CUR_DATETIME.'-'.$result.PHP_EOL, FILE_APPEND);
-					exit();
-				} catch (\Exception $e) {
-					file_put_contents(YEWU_FILE, CUR_DATETIME.'-'.$e->getMessage().PHP_EOL, FILE_APPEND);
-					echo 'error|Exception:'.$e->getMessage();exit();
-				}
-			}else{
-				file_put_contents(YEWU_FILE, CUR_DATETIME.'-Paymethod is null'.PHP_EOL, FILE_APPEND);
-				echo 'error|Paymethod is null';exit();
+		$paymethod = isset($_GET['paymethod'])?$_GET['paymethod']:(isset($_POST['paymethod'])?$_POST['paymethod']:'zfbf2f');
+		$payments = $this->m_payment->getConfig();
+		if(isset($payments[$paymethod]) AND !empty($payments[$paymethod])){
+			try {
+				$payconfig = $payments[$paymethod];
+				$payclass = "\\Pay\\".$paymethod."\\".$paymethod;
+				$PAY = new $payclass();
+				echo $result = $PAY->notify($payconfig);
+				file_put_contents(YEWU_FILE, CUR_DATETIME.'-'.$result.PHP_EOL, FILE_APPEND);
+				exit();
+			} catch (\Exception $e) {
+				file_put_contents(YEWU_FILE, CUR_DATETIME.'-'.$e->getMessage().PHP_EOL, FILE_APPEND);
+				echo 'error|Exception:'.$e->getMessage();exit();
 			}
 		}else{
-			file_put_contents(YEWU_FILE, CUR_DATETIME.'-Method forbidden'.PHP_EOL, FILE_APPEND);
-			echo 'error|Data is null';exit();
+			file_put_contents(YEWU_FILE, CUR_DATETIME.'-Paymethod is null'.PHP_EOL, FILE_APPEND);
+			echo 'error|Paymethod is null';exit();
 		}
-    }
 }
