1dfdbe1ed737a7725ab74d1d8820d20e1f5444c7
莫正强 <43036456@qq.com>
2018-12-17 09:58:52 +0800

    调整图片

--- application/function/F_Img.php
+++ application/function/F_Img.php
@@ -182,3 +182,13 @@ if ( ! function_exists('ImageToJPG')){
 		@imagedestroy($ni); 
 	} 
 }
+
+if ( ! function_exists('base64EncodeImage')){
+	function base64EncodeImage($image_file) {
+	  $base64_image = '';
+	  $image_info = getimagesize($image_file);
+	  $image_data = fread(fopen($image_file, 'r'), filesize($image_file));
+	  $base64_image = 'data:' . $image_info['mime'] . ';base64,' . chunk_split(base64_encode($image_data));
+	  return $base64_image;
+	}
+}
