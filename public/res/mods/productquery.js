cf07f871da9bd6d3c6464ac23c7cebdd221c2ed5
莫正强 <43036456@qq.com>
2018-12-24 17:50:51 +0800

    完善js效果

--- public/res/mods/productquery.js
+++ public/res/mods/productquery.js
@@ -1,9 +1,10 @@
-layui.define(['layer', 'form','jquery','base64','laytpl'], function(exports){
+layui.define(['layer', 'form','jquery','base64','laytpl','element'], function(exports){
 	var $ = layui.jquery;
 	var layer = layui.layer;
 	var form = layui.form;
 	var device = layui.device();
 	var laytpl = layui.laytpl;
+	var element = layui.element;
 	var lodding;
 	
 	function createTime(v){
