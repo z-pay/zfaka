0ea2b7970e8e107a6c84357c17cab879ca9d56cd
莫正强 <43036456@qq.com>
2018-12-25 15:27:48 +0800

    新增数据修复功能

--- application/modules/Admin/controllers/Productscard.php
+++ application/modules/Admin/controllers/Productscard.php
@@ -413,7 +413,41 @@ class ProductscardController extends AdminBasicController
 		echo $content;
 		exit();
 	}
-	
+	public function repairajaxAction()
+	{
+		$method = $this->getPost('method',false);
+		$csrf_token = $this->getPost('csrf_token', false);
+		
+		$data = array();
+		
+        if ($this->AdminUser==FALSE AND empty($this->AdminUser)) {
+            $data = array('code' => 1000, 'msg' => '请登录');
+			Helper::response($data);
+        }
+		
+		if($method AND $csrf_token){
+			if ($this->VerifyCsrfToken($csrf_token)) {
+				$field = array('id','card');
+				$items = $this->m_products_card->Field($field)->Order(array('id'=>'DESC'))->Select();
+				if (empty($items)) {
+					$data = array('code' => 1004, 'msg' => '无数据，不需要修复');
+				} else {
+					foreach($items AS $item){
+						$card = getRawText($item['card'],false);
+						$m = array('card'=>$card);
+						$this->m_products_card->UpdateByID($m,$item['id']);
+						unset($card,$m);
+					}
+					$data = array('code' => 1, 'msg' => '修复完成');
+				}
+			} else {
+                $data = array('code' => 1001, 'msg' => '页面超时，请刷新页面后重试!');
+            }
+		}else{
+			$data = array('code' => 1000, 'msg' => '丢失参数');
+		}
+		Helper::response($data);
+	}		
     private function conditionSQL($param,$alias='')
     {
         $condition = "1";
--- public/res/mods/adminproductscard.js
+++ public/res/mods/adminproductscard.js
@@ -155,6 +155,48 @@ layui.define(['layer', 'table', 'form','upload'], function(exports){
 			})
 		})
 	}); 
+	
+    form.on('submit(repair)', function(data){
+		data.field.csrf_token = TOKEN;
+		data.field.method = 'repair';
+		var i = layer.load(2,{shade: [0.5,'#fff']});
+			$.ajax({
+				url: '/'+ADMIN_DIR+'/productscard/repairajax/',
+				type: 'POST',
+				dataType: 'json',
+				data: data.field,
+			})
+			.done(function(res) {
+				if (res.code == '1') {
+					layer.open({
+						type: 1
+						,title: '修复数据'
+						,offset: 'auto'
+						,id: 'layerPayone' //防止重复弹出
+						,content: '<div style="padding: 20px 100px;">'+res.msg+'</div>'
+						,btn: '关闭'
+						,btnAlign: 'c' //按钮居中
+						,shade: 0.8 //不显示遮罩
+						,yes: function(){
+							location.reload();
+						}
+						,cancel: function(){ 
+							location.reload();
+						} 
+					});
+				} else {
+					layer.msg(res.msg,{icon:2,time:5000});
+				}
+			})
+			.fail(function() {
+				layer.msg('服务器连接失败，请联系管理员',{icon:2,time:5000});
+			})
+			.always(function() {
+				layer.close(i);
+			});
+
+			return false; //阻止表单跳转。如果需要表单跳转，去掉这段即可。
+    });	
 	//添加
 	form.on('submit(add)', function(data){
 		data.field.csrf_token = TOKEN;
