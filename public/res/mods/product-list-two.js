b06312e729252cfe249054604b8de8b4bff0b323
莫正强 <43036456@qq.com>
2019-01-01 17:33:44 +0800

    首页添加流媒体

--- public/res/mods/product-list-two.js
+++ public/res/mods/product-list-two.js
@@ -1,14 +1,15 @@
-layui.define(['layer','jquery','laytpl','element'], function(exports){
+layui.define(['layer','jquery','laytpl','element','flow'], function(exports){
 	var $ = layui.jquery;
 	var layer = layui.layer;
 	var laytpl = layui.laytpl;
 	var element = layui.element;
-
-	function getProduct()
+	var flow = layui.flow;
+	
+	function getProduct(p)
 	{ 
 		var i = layer.load(2,{shade: [0.5,'#fff']});
 		$.ajax({
-			url: '/product/get/',
+			url: '/product/get/?page='+p,
 			type: 'POST',
 			dataType: 'json',
 			data: {"tid": 0},
@@ -49,6 +50,23 @@ layui.define(['layer','jquery','laytpl','element'], function(exports){
 			,content: '<div style="padding: 50px; line-height: 22px; background-color: #393D49; color: #fff; font-weight: 300;">'+layerad+'</div>'
 		});
 	}
-	getProduct();
+	
+	getProduct(1);
+	
+	//流媒体
+	flow.load({
+		elem: '#product-list-two-view'
+		,done: function(page, next){
+			var lis = [];
+			$.get('/product/get/?page='+page, function(res){
+				var getTpl = product_list_two_tpl.innerHTML;
+				laytpl(getTpl).render(res, function(html){
+					lis.push(html);
+				});
+				next(lis.join(''), page < res.count);    
+			});
+		}
+	});
+	
 	exports('product-list-two',null)
 });
