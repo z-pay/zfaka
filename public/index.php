3355aafcdb165d230e11f99516a30e52f2889fa9
莫正强 <43036456@qq.com>
2019-01-16 09:45:50 +0800

    完善ZFAKA安装检测

--- public/index.php
+++ public/index.php
@@ -2,6 +2,10 @@
 header('content-Type:text/html;charset=utf-8;');
 define('APP_PATH',  dirname(dirname(__FILE__)));
 \Yaf\Loader::import(APP_PATH.'/application/init.php');
-$app = new \Yaf\Application(APP_PATH.'/conf/application.ini');
-$app->getDispatcher()->throwException(TRUE);
-$app->bootstrap()->run();
+if(!file_exists(INSTALL_LOCK)){
+	include_once( dirname(__FILE__)."/init.php");
+}else{
+	$app = new \Yaf\Application(APP_PATH.'/conf/application.ini');
+	$app->getDispatcher()->throwException(TRUE);
+	$app->bootstrap()->run();
+}
