2491ff3ea0f3cd75d01f36b8ce4be482035d6ccb
莫正强 <43036456@qq.com>
2019-01-16 18:54:16 +0800

    修改判断逻辑

--- public/index.php
+++ public/index.php
@@ -4,9 +4,8 @@ define('APP_PATH',  dirname(dirname(__FILE__)));
 
 if(!file_exists(APP_PATH.'/install/install.lock')){
 	include_once( dirname(__FILE__)."/init.php");
-}else{
-	\Yaf\Loader::import(APP_PATH.'/application/init.php');
-	$app = new \Yaf\Application(APP_PATH.'/conf/application.ini');
-	$app->getDispatcher()->throwException(TRUE);
-	$app->bootstrap()->run();
 }
+\Yaf\Loader::import(APP_PATH.'/application/init.php');
+$app = new \Yaf\Application(APP_PATH.'/conf/application.ini');
+$app->getDispatcher()->throwException(TRUE);
+$app->bootstrap()->run();
