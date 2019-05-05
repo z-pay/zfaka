8b11a378a3f3a8fd5ac099e44860c1096ae9b5cc
莫正强 <43036456@qq.com>
2019-05-05 08:23:36 +0800

    修改相关说明

--- install/faka.sql
+++ install/faka.sql
@@ -81,7 +81,7 @@ INSERT INTO `t_config` (`id`, `catid`, `name`, `value`, `tag`, `lock`, `updateti
 (4, 1, 'weburl', 'http://faka.zlkb.net', '当前网站地址,用于支付站点异步返回，务必修改正确', 1, 1453452674),
 (5, 1, 'adminemail', 'demo@demo.com', '管理员邮箱,用于接收邮件提醒用', 1, 1453452674),
 (6, 1, 'webname', 'ZFAKA平台', '当前站点名称', 1, 1453452674),
-(7, 1, 'webdescription', '本系统由资料空白开发', '当前站点描述', 1, 1453452674),
+(7, 1, 'webdescription', '本系统由资料空白开发并免费提供', '当前站点描述', 1, 1453452674),
 (8, 1, 'notice', '本系统商品均可正常购买。开源下载地址：github地址:&lt;a href=&quot;https://github.com/zlkbdotnet/zfaka/&quot; target=&quot;_blank&quot;&gt;https://github.com/zlkbdotnet/zfaka/&lt;/a&gt;', '首页公告', 1, 1453452674),
 (9, 1, 'ad', '&lt;image src=&quot;/res/images/pay/supportme.jpg&quot;&gt;', '购买页默认内容', 1, 1453452674),
 (10, 1, 'yzmswitch', '1', '验证码开关(1开，0关)', 1, 1453452674),
