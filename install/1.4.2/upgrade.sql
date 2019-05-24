9ac91756f70a93fce9d54dbf24e26bafba1cae7a
莫正强 <43036456@qq.com>
2019-05-24 16:45:33 +0800

    进行一系列功能与页面调整

--- install/1.4.2/upgrade.sql
+++ install/1.4.2/upgrade.sql
@@ -1,1 +1,4 @@
 INSERT INTO `t_config` (`id`, `catid`, `name`, `value`, `tag`, `lock`, `updatetime`) VALUES (33, 1, 'tpl', 'hyacinth', '全新的整站模版', '1', '1546063186');
+DELETE FROM `t_config` WHERE `t_config`.`id` = 29;
+DELETE FROM `t_config` WHERE `t_config`.`id` = 19;
+DELETE FROM `t_config` WHERE `t_config`.`id` = 12;
