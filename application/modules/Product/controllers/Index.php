8f56f321c5e61955be86a15c4b6da7f7e9178b90
莫正强 <43036456@qq.com>
2019-05-26 10:26:53 +0800

    优化数据输出

--- application/modules/Product/controllers/Index.php
+++ application/modules/Product/controllers/Index.php
@@ -20,17 +20,20 @@ class IndexController extends ProductBasicController
     {
 		if(file_exists(INSTALL_LOCK)){
 			$data = array();
-			//获取分类
+			//获取原始分类
 			$products_type = $this->m_products_type->Where(array('active'=>1,'isdelete'=>0))->Order(array('sort_num' => 'DESC'))->Select();
 			$data['products_type'] = $products_type;
-			//获取商品
-			$sql = "SELECT p1.*,p2.name AS products_type_name FROM `t_products` as p1 left join t_products_type as p2 on p1.typeid =p2.id where p1.active=1 and p1.isdelete=0 order by p2.sort_num DESC, p1.sort_num DESC";
+			//获取有效的商品与有效的商品分类
+			$products_type_active = array();
+			$sql = "SELECT p1.*,p2.id AS products_type_id,p2.name AS products_type_name FROM `t_products` as p1 left join t_products_type as p2 on p1.typeid =p2.id where p1.active=1 and p1.isdelete=0 order by p2.sort_num DESC, p1.sort_num DESC";
 			$items = $this->m_products->Query($sql);
 			if (empty($items)) {
 				$data['products'] = array();
+				$data['products_type_active'] = array();
 			} else {
 				//对密码与库存做特别处理
 				foreach($items AS $k=>$p){
+					$products_type_active[$p['products_type_id']] = array('id'=>$p['products_type_id'],'name'=>$p['products_type_name']);
 					if(isset($p['password']) AND strlen($p['password'])>0){
 						$items[$k]['password'] = "hidden";
 					}
@@ -38,6 +41,7 @@ class IndexController extends ProductBasicController
 						$items[$k]['qty'] = $p['qty_virtual'];
 					}
 				}
+				$data['products_type_active'] = $products_type_active;
 				$data['products'] = $items;
 			}
 			
