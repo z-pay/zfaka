8ed931095f14ab57646c95ed5cbbfdbf5ab3535e
莫正强 <43036456@qq.com>
2019-05-30 15:39:11 +0800

    更新

--- public/res/mods/adminemail.js
+++ public/res/mods/adminemail.js
@@ -11,9 +11,9 @@ layui.define(['layer', 'form', 'table'], function(exports){
 		cellMinWidth:60,
 		cols: [[
 			{field: 'id', title: 'ID', width:80},
-			{field: 'mailaddress', title: '邮箱账号'},
+			{field: 'sendmail', title: '邮箱账号'},
 			{field: 'sendname', title: '发件人', minWidth:200},
-			{field: 'isssl', title: '使用SSL', width:100, templet: '#isssl',align:'center'},
+			{field: 'protocol', title: '协议', width:100, align:'center'},
 			{field: 'port', title: '端口', width:100, align:'center'},
 			{field: 'isactive', title: '是否激活', width:100, templet: '#isactive',align:'center'},
 			{field: 'opt', title: '操作', width:120, templet: '#opt',align:'center'}
