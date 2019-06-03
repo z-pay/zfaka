a6b020a0ac43cb277ea2a36946064d01226eeb96
莫正强 <43036456@qq.com>
2019-06-03 11:59:22 +0800

    修复邮箱大写时判断异常的BUG

--- application/modules/Product/controllers/Order.php
+++ application/modules/Product/controllers/Order.php
@@ -51,8 +51,13 @@ class OrderController extends ProductBasicController
 						$qq = '';
 					}else{
 						$qq = $this->getPost('qq');
-						if($qq AND is_numeric($qq)){
-							$email = $qq.'@qq.com';
+						if($qq){
+							if(is_numeric($qq)){
+								$email = $qq.'@qq.com';
+							}else{
+								$data = array('code' => 1006, 'msg' => 'QQ格式不正确');
+								Helper::response($data);	
+							}
 						}else{
 							$data = array('code' => 1006, 'msg' => '丢失参数');
 							Helper::response($data);
@@ -60,8 +65,14 @@ class OrderController extends ProductBasicController
 					}
 				}else{
 					$email = $this->getPost('email',false);
-					if($email AND isEmail($email)){
-						$qq = '';
+					if($email){
+						$email = strtolower($email);
+						if(isEmail($email)){
+							$qq = '';
+						}else{
+							$data = array('code' => 1006, 'msg' => '邮箱格式不正确');
+							Helper::response($data);
+						}
 					}else{
 						$data = array('code' => 1006, 'msg' => '丢失参数');
 						Helper::response($data);
